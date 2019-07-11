ebe3fb7a8690adbff6485a19b8967d396dd0b346
brad medeiros <bradmedeiros0@gmail.com>
2019-07-11 00:41:49 -0700

    loadFile

--- src/shaders.cpp
+++ src/shaders.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
 #include "./shaders.h"
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <glad/glad.h>
+#include <stdexcept>
 
 std::string loadFile(std::string filepath){
-   return std::string("hello world");
+   std::ifstream file(filepath.c_str());
+   if (!file.good()){
+     throw std::runtime_error("file not found");
+   }
+   
+   std::stringstream buffer;
+   buffer << file.rdbuf();
+   return buffer.str();
+}
+
+
+void loadShader(std::string vertexShaderFilepath, std::string fragmentShaderFilepath){
+   std::string vertexShader = loadFile(vertexShaderFilepath);
+   std::cout << "vertex shader content is: " << vertexShader << std::endl;
+   //unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER); 
 }
