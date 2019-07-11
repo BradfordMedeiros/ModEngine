0ac43f49d087cd603f50d84078da5e2866ac6fed
brad medeiros <bradmedeiros0@gmail.com>
2019-07-10 22:12:08 -0700

    basic hello world window

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,24 @@
 #include <iostream>
 #include <GLFW/glfw3.h>
 #include <glad/glad.h>
+#include <unistd.h>
 
 int main(){
-  std::cout << "hello world" << std::endl; 
-
   glfwInit();
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
+  GLFWwindow* window = glfwCreateWindow(800, 600, "ModEngine", NULL, NULL);
+  if (window == NULL){
+    std::cerr << "Error: failed to create window" << std::endl;
+    glfwTerminate();
+    return -1;
+  } 
+  glfwMakeContextCurrent(window); 
+  
+  while (!glfwWindowShouldClose(window)){
+    glfwPollEvents(); 
+  } 
   return 0;
 }
