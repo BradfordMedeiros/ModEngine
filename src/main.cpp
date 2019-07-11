3efc1dedb17e6ad9fc4b18c9681e672ec334dd6c
brad medeiros <bradmedeiros0@gmail.com>
2019-07-10 22:46:33 -0700

    register framebuffer resize callback

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,17 @@
 #include <GLFW/glfw3.h>
 #include <glad/glad.h>
 
+#define INITIAL_SCREEN_WIDTH 800
+#define INITIAL_SCREEN_HEIGHT 600
+
 void handleInput(GLFWwindow *window){
    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS){
      	glfwSetWindowShouldClose(window, true);
    }
+}
+
+void onFramebufferSizeChange(GLFWwindow* window, int width, int height){
+  std::cout << "EVENT: framebuffer resized.  new size:  " << "width("<< width << ")" << " height(" << height << ")" << std::endl; 
 }
 
 int main(){
@@ -14,14 +21,14 @@ int main(){
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-  GLFWwindow* window = glfwCreateWindow(800, 600, "ModEngine", NULL, NULL);
+  GLFWwindow* window = glfwCreateWindow(INITIAL_SCREEN_WIDTH, INITIAL_SCREEN_HEIGHT, "ModEngine", NULL, NULL);
   if (window == NULL){
-    std::cerr << "Error: failed to create window" << std::endl;
+    std::cerr << "ERROR: failed to create window" << std::endl;
     glfwTerminate();
     return -1;
   } 
   glfwMakeContextCurrent(window); 
-  
+  glfwSetFramebufferSizeCallback(window, onFramebufferSizeChange); 
   while (!glfwWindowShouldClose(window)){
     handleInput(window);
     glfwPollEvents();
