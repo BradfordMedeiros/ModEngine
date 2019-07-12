647a2a2af0e5a5fc566186bfd0f1ceb408cc1539
brad medeiros <bradmedeiros0@gmail.com>
2019-07-12 00:15:21 -0700

    -

--- src/shaders.h
+++ src/shaders.h
@@ -1,5 +1,5 @@
-#ifndef SHADERS
-#define SHADERS 
+#ifndef MOD_SHADERS
+#define MOD_SHADERS 
 #include <iostream>
 
 unsigned int loadShader(std::string fragmentShaderFilepath, std::string vertexShaderFilepath);
