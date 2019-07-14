bdf0cd26d78ebd3f40fddab474646060b6d3ac09
brad medeiros <bradmedeiros0@gmail.com>
2019-07-13 19:22:23 -0700

    setViewport, set current screen dim on framebuffer size change

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 
 #include <glm/glm.hpp>
 #include <glm/glm.hpp>
-//#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
 #include "./shaders.h"
@@ -23,6 +23,11 @@ void onMouseEvents(GLFWwindow* window, double xPos, double yPos){
    std::cout << "CONTROL: mouse: xpos(" << xPos << ") yPos(" << yPos << ")" << std::endl; 
 }
 
+unsigned int currentScreenWidth = INITIAL_SCREEN_WIDTH;
+unsigned int currentScreenHeight = INITIAL_SCREEN_HEIGHT;
+
+glm::mat4 projection;
+
 int main(int argc, char* argv[]){
   if (argc < 2){
     std::cerr << "please provide texture file location" << std::endl;
@@ -30,7 +35,6 @@ int main(int argc, char* argv[]){
   }
   options opts = loadOptions(argv[1]);
 
- 
   std::cout << "LIFECYCLE: program starting" << std::endl;
   glfwInit();
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
@@ -38,7 +42,7 @@ int main(int argc, char* argv[]){
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
  
-  GLFWwindow* window = glfwCreateWindow(INITIAL_SCREEN_WIDTH, INITIAL_SCREEN_HEIGHT, "ModEngine", NULL, NULL);
+  GLFWwindow* window = glfwCreateWindow(currentScreenWidth, currentScreenHeight, "ModEngine", NULL, NULL);
   if (window == NULL){
     std::cerr << "ERROR: failed to create window" << std::endl;
     glfwTerminate();
@@ -51,21 +55,25 @@ int main(int argc, char* argv[]){
      glfwTerminate();
      return -1;
   }
-
+ 
   auto onFramebufferSizeChange = [](GLFWwindow* window, int width, int height) -> void {
      std::cout << "EVENT: framebuffer resized:  new size-  " << "width("<< width << ")" << " height(" << height << ")" << std::endl;
-     glViewport(0, 0, width, height);
+     currentScreenWidth = width;
+     currentScreenHeight = height;
+     glViewport(0, 0, currentScreenWidth, currentScreenHeight);
+     projection = glm::perspective(glm::radians(45.0f), (float)currentScreenWidth / currentScreenHeight, 0.1f, 100.0f); 
   }; 
 
+  onFramebufferSizeChange(window, currentScreenWidth, currentScreenHeight);
   glfwSetFramebufferSizeCallback(window, onFramebufferSizeChange); 
-  glViewport(0, 0, INITIAL_SCREEN_WIDTH, INITIAL_SCREEN_HEIGHT); 
- 
+  
   std:: cout << "shader file path is " << opts.shaderFolderPath << std::endl;
   unsigned int shaderProgram = loadShader(opts.shaderFolderPath + "/vertex.glsl", opts.shaderFolderPath + "/fragment.glsl");
 
   glm::mat4 model = glm::mat4(1.0f);
-  glm::mat4 view = glm::mat4(1.0f);
-  glm::mat4 projection = glm::mat4(1.0f);
+  glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -3.0f)); 
+  
+  onFramebufferSizeChange(window, currentScreenWidth, currentScreenHeight); 
 
   glUseProgram(shaderProgram); 
 
@@ -84,7 +92,6 @@ int main(int argc, char* argv[]){
     glClear(GL_COLOR_BUFFER_BIT);
     drawMesh(vaopointer);
    
-    //
     model = glm::rotate(model, glm::radians(1.0f), glm::vec3(0.0, 1.0, 0.0));
   }
 
