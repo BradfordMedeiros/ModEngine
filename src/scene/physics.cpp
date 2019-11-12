9eb7ada59e2c68ad73b4c5c64b18956a9fc99922
brad medeiros <bradmedeiros0@gmail.com>
2019-11-12 00:11:14 -0800

    rotations seem to work, needs some testing

--- src/scene/physics.cpp
+++ src/scene/physics.cpp
@@ -7,7 +7,7 @@ btVector3 glmToBt(glm::vec3 pos){
   return btVector3(pos.x, pos.y, pos.z);
 }
 glm::quat btToGlm(btQuaternion rotation){
-  return glm::quat(rotation.getX(), rotation.getY(), rotation.getZ(), rotation.getW());
+  return glm::quat(rotation.getW(), rotation.getX(), rotation.getY(), rotation.getZ());
 }
 btQuaternion glmToBt(glm::quat rotation){
   return btQuaternion(rotation.x, rotation.y, rotation.z, rotation.w);
