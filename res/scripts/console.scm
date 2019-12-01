42ad335136d002c4b3fe60b2e608126f4a32cf4a
brad medeiros <bradmedeiros0@gmail.com>
2019-11-30 20:29:32 -0800

    modterminal script

---
+++ res/scripts/console.scm
@@ -0,0 +1,74 @@
+
+(define (onMouse button action mods) (display ""))
+(define (onObjSelected selectedIndex) (display ""))
+
+
+(define lineHistory #())
+(define (logToHistory lineText)
+  (display (string-append "line text length is " (number->string (string-length lineText))))
+  (cond 
+    ((equal? lineText "CLEAR")  (set! lineHistory #()))
+    ((equal? lineText "QUIT") (exit))
+    (#t (set! lineHistory (list->vector (reverse (cons lineText (reverse (vector->list lineHistory)))))))
+  )
+)
+
+(define currentLineBuffer "")
+(define (appendToBuffer value)
+  (set! currentLineBuffer (string-append currentLineBuffer value))
+)
+(define (submitBuffer)
+  (logToHistory currentLineBuffer)
+  (set! currentLineBuffer "")
+)
+(define (backspaceBuffer)
+  (set! currentLineBuffer (substring currentLineBuffer 0 (max 0 (- (string-length currentLineBuffer) 1))))
+)
+
+
+(define (drawFrame numLines fontSize offsetY)
+  (define currentHeight 0)
+  (drawText "ModTerminal" 10 (+ offsetY currentHeight) fontSize)
+  (set! currentHeight (+ currentHeight 10))
+  (drawText "--------------------------------------" 10 (+ offsetY currentHeight) 1)
+  (do ((row (- numLines 1) (- row 1))) ((< row 0))
+    (let* ((historyLength (vector-length lineHistory)) (lineIndex (- historyLength row)))
+      (if (and (< lineIndex historyLength) (>= lineIndex 0))
+        (begin 
+          (set! currentHeight (+ currentHeight 10))
+          (drawText (vector-ref lineHistory lineIndex) 10 (+ offsetY currentHeight) fontSize)
+        )
+      )
+    )
+  )
+  (set! currentHeight (+ currentHeight 10))
+  (drawText "//////////////////////////////" 10 (+ offsetY currentHeight) 2)
+  (set! currentHeight (+ currentHeight 10))
+  (drawText currentLineBuffer 10 (+ offsetY currentHeight) fontSize)
+  (set! currentHeight (+ currentHeight 10))
+  (drawText "//////////////////////////////" 10 (+ offsetY currentHeight) 2)
+)
+
+
+(define (onFrame)
+  (if showConsole
+    (drawFrame 10 4 100)
+  )
+)
+
+(define showConsole #f)
+(define (onKey key scancode action mods)
+  (if (and (= key 96) (= action 1))
+    (set! showConsole (not showConsole))
+  )
+  (if showConsole
+    (if (and (= action 1) (not (= key 96)))
+      (cond
+        ((= key 257) (submitBuffer)) 
+        ((= key 259) (backspaceBuffer))
+        (#t (appendToBuffer (string (integer->char key))))
+      )
+    )
+  )
+)
+
