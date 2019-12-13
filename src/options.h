73116aee3e55b2f21549c03022bca0aa043615d8
brad medeiros <bradmedeiros0@gmail.com>
2019-12-13 00:19:07 -0800

    placeholder options files

---
+++ src/options.h
@@ -0,0 +1,10 @@
+#ifndef MOD_OPTIONS
+#define MOD_OPTIONS
+
+// @TODO 
+// the point of this file is as placeholder for global settings tha
+// will be able to be changed dynamically (from scheme ultimately, probably also bound initially to argv)
+// so userland code can do things like set fov, anti-aliasing enable/disable/etc 
+
+
+#endif
