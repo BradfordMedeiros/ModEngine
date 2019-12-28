1c08707bb4f4e1ac34b2b29e6b2617002a5285db
brad medeiros <bradmedeiros0@gmail.com>
2019-12-27 23:46:29 -0800

    generate chunks specialized around generating unit tile floors of n dimension, now supports size

--- res/tools/generate-chunks.py
+++ res/tools/generate-chunks.py
@@ -17,15 +17,29 @@ def generate_file_extension_list(num_chunks_width, num_chunks_height, num_chunks
 
   return file_names
 
-def write_content(size, output_directory, data):
-  for file_extension in generate_file_extension_list(size, size, size):
+
+def content_for_filename(filename, chunksize):
+  values = list(map(lambda value: int(value), filename.split('.')))
+  template_file = ""
+  x_pos = chunksize * values[0]
+  y_pos = chunksize * values[1] + chunksize / 2
+  z_pos = chunksize * values[2] 
+  template_file = template_file + "unitcube:position:" + str(x_pos) + " " + str(y_pos) + " " + str(z_pos) + "\n"
+  template_file = template_file + "unitcube:scale:" + str(chunksize) + " 1.0 " + str(chunksize) + "\n"
+  template_file = template_file + "unitcube:mesh:./res/models/unit_rect/unit_rect.obj" + "\n"
+  return template_file
+
+
+def write_content(num, size, output_directory, data):
+  for file_extension in generate_file_extension_list(num, num, num):
     with open(os.path.join(output_directory, file_extension), 'w') as filehandle:
-      filehandle.write(data + '\n')
+      filehandle.write(content_for_filename(file_extension, size))
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--output', '-o', type=str, default='.', help='Directory to output generated chunk files')
-parser.add_argument('--size', '-s', type=int, default=1, help='Number of chunks to make')
-parser.add_argument('--data', '-d', type=str, default='', help='Data to insert into each chunk file')
+parser.add_argument('--num', '-n', type=int, default=1, help='Number of chunks to make')
+parser.add_argument('--size', '-s', type=int, default=100, help='Size of the chunk (assumes x,y,z values are equal)')
 args = parser.parse_args()
 
-write_content(args.size, args.output, args.data)
+write_content(args.num, args.size, args.output)
+
