c21f736d557679c4655b4dc0192482951d14361d
brad medeiros <bradmedeiros0@gmail.com>
2019-12-30 23:43:15 -0800

    dynamic light sources able to be passed into shader

--- res/shaders/default/fragment.glsl
+++ res/shaders/default/fragment.glsl
@@ -1,4 +1,5 @@
 #version 330 core
+
 out vec4 FragColor;
 in vec3 FragPos;
 in vec2 TexCoord;
@@ -11,6 +12,11 @@ uniform vec3 lightPosition;
 uniform bool enableDiffuse;
 uniform bool enableSpecular;
 
+#define MAX_LIGHTS 128
+uniform int numlights;
+uniform vec3 lights[MAX_LIGHTS];
+
+
 void main(){
   if (tint.r < 0.1){
     FragColor = vec4(tint.r, tint.g, tint.b, 1.0);
@@ -20,22 +26,27 @@ void main(){
       discard;
     }
 
-    vec3 lightDir = normalize(lightPosition - FragPos);
-    vec3 normal = normalize(Normal);
+    vec3 ambient = vec3(0.2, 0.2, 0.2);     
+    vec3 totalSpecular = vec3(0, 0, 0);
+    vec3 totalDiffuse  = vec3(0, 0, 0);
+    
+    for (int i = 0; i < min(numlights, MAX_LIGHTS); i++){
+        vec3 lightPos = lights[i];
+        vec3 lightDir = normalize(lightPosition - FragPos);
+        vec3 normal = normalize(Normal);
 
-    // Ambient lighting
-    vec3 ambient = vec3(0.2, 0.2, 0.2);
+        vec3 diffuse = max(dot(normal, lightDir), 0.0) * vec3(1.0, 1.0, 1.0);
 
-    // Diffuse lighting
-    vec3 diffuse = max(dot(normal, lightDir), 0.0) * vec3(1.0, 1.0, 1.0);
+        vec3 viewDir = normalize(cameraPosition - FragPos);
+        vec3 reflectDir = reflect(-lightDir, normal);  
+        vec3 specular = pow(max(dot(viewDir, reflectDir), 0.0), 32) * vec3(1.0, 1.0, 1.0);  
+ 
+        totalDiffuse = totalDiffuse + diffuse;
+        totalSpecular = totalSpecular + specular;
+    }
 
-    // Specular lighting
-    vec3 viewDir = normalize(cameraPosition - FragPos);
-    vec3 reflectDir = reflect(-lightDir, normal);  
-    vec3 specular = pow(max(dot(viewDir, reflectDir), 0.0), 32) * vec3(1.0, 1.0, 1.0);
-
-    vec3 diffuseValue = enableDiffuse ? diffuse : vec3(0, 0, 0);
-    vec3 specularValue = enableSpecular ? specular : vec3(0, 0, 0);
+    vec3 diffuseValue = enableDiffuse ? totalDiffuse : vec3(0, 0, 0);
+    vec3 specularValue = enableSpecular ? totalSpecular : vec3(0, 0, 0);
     vec4 color = vec4(ambient + diffuseValue + specularValue, 1.0) * texColor;
     FragColor = color * vec4(tint.x, tint.y, tint.z, 1.0);
   }
