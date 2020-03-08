9b0bfeb81d01d049ab98743d9b4cb2cbbba7a5af
brad medeiros <bradmedeiros0@gmail.com>
2020-03-07 16:26:43 -0800

    animation debug print statements

--- src/animation.cpp
+++ src/animation.cpp
@@ -1,6 +1,7 @@
 #include "./animation.h"
 
-void printAnimationInfo(float currentTime, float elapsedTime, Animation& animation){
+void printAnimationInfo(Animation& animation, float currentTime, float elapsedTime, float currentTick){
+  std::cout << "-animation info-" << std::endl;
   std::cout << "playing animation: " << animation.name << std::endl;
   std::cout << "current time: " << currentTime << std::endl;
   std::cout << "elapsed time: " << elapsedTime << std::endl;
@@ -10,11 +11,20 @@ void printAnimationInfo(float currentTime, float elapsedTime, Animation& animati
   std::cout << "num ticks: " << animation.duration << std::endl;
   std::cout << "ticks/s " << animation.ticksPerSecond << std::endl;
   std::cout << "num channels: " << animation.channels.size() << std::endl;
+  std::cout << "current tick: " << currentTick << std::endl;
+}
 
-  std::cout << "node name: " << animation.channels.at(0).nodeName << std::endl;
-  std::cout << "num chan0 pos keys: " << animation.channels.at(0).positionKeys.size() << std::endl;
-  std::cout << "num chan0 rot keys: " << animation.channels.at(0).rotationKeys.size() << std::endl;
-  std::cout << "num chan0 scale keys: " << animation.channels.at(0).scalingKeys.size() << std::endl; 
+void printChannelInfo(AnimationChannel& channel, int tickPosIndex, int tickRotIndex, int tickScaleIndex){
+  std::cout << "-channel info-" << std::endl; 
+  std::cout << "node name: " << channel.nodeName << std::endl;
+  std::cout << "num chan0 pos keys: " << channel.positionKeys.size() << std::endl;
+  std::cout << "num chan0 rot keys: " << channel.rotationKeys.size() << std::endl;
+  std::cout << "num chan0 scale keys: " << channel.scalingKeys.size() << std::endl; 
+
+  std::cout << "tick pos key: " << tickPosIndex << std::endl;
+  std::cout << "tick rot key: " << tickRotIndex << std::endl;
+  std::cout << "tick scale key: " << tickScaleIndex << std::endl;
+  std::cout << std::endl;
 }
 
 // slightly off, check comparison func, maybe to do with the float values idk.
@@ -45,29 +55,25 @@ void animateNode(std::string nodeName, aiVectorKey& positionKey, aiQuatKey& rota
 
 }
 
+void advanceAnimationForNode(Animation& animation, AnimationChannel& channel, float currentTick, float currentTime, float elapsedTime){
+  auto tickPosIndex = findIndexForKey(channel.positionKeys, currentTick);
+  auto tickRotIndex = findIndexForKey(channel.rotationKeys, currentTick);
+  auto tickScaleIndex = findIndexForKey(channel.scalingKeys, currentTick);
 
-void advanceAnimation(Animation& animation, float currentTime, float elapsedTime){
-  printAnimationInfo(currentTime, elapsedTime, animation);
-
-  // 200 ticks / 100 ticks per second = 2 seconds
-  assert(animation.ticksPerSecond != 0);  // some models can have 0 ticks, probably should just set a default rate for these
-
-  auto currentTick = currentTime * animation.ticksPerSecond;   
-  std::cout << "current tick: " << currentTick << std::endl;
-  
-  auto tickPosIndex = findIndexForKey(animation.channels.at(0).positionKeys, currentTick);
-  std::cout << "tick pos key: " << tickPosIndex << std::endl;
-
-  auto tickRotIndex = findIndexForKey(animation.channels.at(0).rotationKeys, currentTick);
-  std::cout << "tick rot key: " << tickRotIndex << std::endl;
-
-  auto tickScaleIndex = findIndexForKey(animation.channels.at(0).scalingKeys, currentTick);
-  std::cout << "tick scale key: " << tickScaleIndex << std::endl;
+  printChannelInfo(channel, tickPosIndex, tickRotIndex, tickScaleIndex);
 
   animateNode(
-    animation.channels.at(0).nodeName,  
-    animation.channels.at(0).positionKeys.at(tickPosIndex), 
-    animation.channels.at(0).rotationKeys.at(tickRotIndex), 
-    animation.channels.at(0).scalingKeys.at(tickScaleIndex)
+    channel.nodeName,  
+    channel.positionKeys.at(tickPosIndex), 
+    channel.rotationKeys.at(tickRotIndex), 
+    channel.scalingKeys.at(tickScaleIndex)
   );
+}
+
+void advanceAnimation(Animation& animation, float currentTime, float elapsedTime){
+  assert(animation.ticksPerSecond != 0);                                                      // some models can have 0 ticks, probably should just set a default rate for these
+
+  auto currentTick = currentTime * animation.ticksPerSecond;                                  // 200 ticks / 100 ticks per second = 2 seconds
+  printAnimationInfo(animation, currentTime, elapsedTime, currentTick);
+  advanceAnimationForNode(animation, animation.channels.at(0), currentTick, currentTime, elapsedTime);
 }
