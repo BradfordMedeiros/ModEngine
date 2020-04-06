d562ee1cd8194cd6bb8fb164e7a08d3a0d68103d
brad medeiros <bradmedeiros0@gmail.com>
2020-04-05 21:30:36 -0700

    remove internal functions from header in playback

--- src/scene/animation/playback.h
+++ src/scene/animation/playback.h
@@ -7,8 +7,6 @@
 #include "../common/mesh.h"
 #include "./animation.h"
 
-glm::mat4 getParentBone(std::map<std::string, std::string>& boneToParent, std::map<std::string, glm::mat4>& nodeToTransformedPose, std::string boneName);
-void processNewPoseOnMesh(std::map<std::string, glm::mat4>& nodeToTransformedPose, std::string boneName, glm::mat4 newPose, NameAndMesh& meshData);
 void playbackAnimation(Animation& animation, std::map<std::string, std::map<std::string, std::string>>& meshnameToBoneToParent, NameAndMesh& meshNameToMeshes, float currentTime, float elapsedTime);
 
 #endif
