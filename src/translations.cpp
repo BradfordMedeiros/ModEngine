a85d2930231cfc246b582063453caaae0e66bdc6
brad medeiros <bradmedeiros0@gmail.com>
2020-04-12 23:01:16 -0700

    camera now renders from point of view of the camera (using it's model position inverse)

--- src/translations.cpp
+++ src/translations.cpp
@@ -1,21 +1,22 @@
 #include "./translations.h"
 
-glm::quat setFrontDelta(glm::quat orientation, float deltaYaw, float deltaPitch, float deltaRoll, float delta){
-  glm::quat yawRotation = glm::rotate(orientation, glm::radians(deltaYaw * delta), glm::vec3(0.0f, 1.0f, 0.0f));
-  glm::quat rotatedYaw = glm::rotate(yawRotation, glm::radians(deltaPitch * delta), glm::vec3(1.0f, 0.0f, 0.0f) * yawRotation);
-  glm::quat rotated = glm::rotate(rotatedYaw, glm::radians(deltaRoll * delta), glm::vec3(0.0f, 0.0f, 1.0f) * rotatedYaw);
-  return rotated;
+glm::quat setFrontDelta(glm::quat orientation, float deltaYaw, float deltaPitch, float deltaRoll, float delta){ // Might be worth considering the ordering of this. 
+  glm::quat pitch  = angleAxis(deltaPitch * delta, glm::vec3(1.0f, 0.0f, 0.0f)) ;
+  glm::quat yaw = glm::angleAxis(deltaYaw * delta,  glm::vec3(0.0f, 1.0f, 0.0f));
+  glm::quat roll = glm::angleAxis(deltaRoll * delta,  glm::vec3(0.0f, 0.0f, 1.0f));
+  return roll * yaw * orientation * pitch;                                      
 }
 
 glm::vec3 moveRelative(glm::vec3 position, glm::quat orientation, glm::vec3 offset, bool xzPlaneOnly){
-  glm::vec3 delta = offset * orientation;
+  glm::vec3 delta = orientation * offset;
   return position + (xzPlaneOnly ? glm::vec3(delta.x, 0, delta.z) : delta);   // @TODO This loses magnitude if you do have upwards velocity.  This should really be just projecting offset onto xz plane
 }
 glm::vec3 move(glm::vec3 position, glm::vec3 offset){
   return glm::vec3(position.x + offset.x, position.y + offset.y, position.z + offset.z);
 }
 glm::mat4 renderView(glm::vec3 position, glm::quat orientation){
-  return glm::translate(glm::toMat4(orientation), glm::vec3(position.x * -1, position.y * - 1, position.z * -1));
+  glm::mat4 cameraModelMatrix =  glm::translate(glm::mat4(1.f), glm::vec3(position.x, position.y, position.z)) * glm::toMat4(orientation);
+  return glm::inverse(cameraModelMatrix);
 }
 
 glm::vec3 getVecAxis(ManipulatorAxis axis){
