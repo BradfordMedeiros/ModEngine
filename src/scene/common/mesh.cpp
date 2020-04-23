e36dbe9f6d100e36a0eb9a8323058a7ad47b123f
brad medeiros <bradmedeiros0@gmail.com>
2020-04-22 21:24:23 -0700

    fix texture loading image bug

--- src/scene/common/mesh.cpp
+++ src/scene/common/mesh.cpp
@@ -185,6 +185,7 @@ Texture loadTexture(std::string textureFilePath){
     format = GL_RGBA;
   }
   
+  glPixelStorei(GL_UNPACK_ALIGNMENT, 1);  // https://stackoverflow.com/questions/15983607/opengl-texture-tilted
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT); 
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
