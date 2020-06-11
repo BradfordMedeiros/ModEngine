100a78601e3cefdd67d7a276de398e65fd51a64f
brad medeiros <bradmedeiros0@gmail.com>
2020-06-10 21:52:50 -0700

    [test scenes] - tcp messaging

---
+++ res/scripts/tcp-test1.scm
@@ -0,0 +1,15 @@
+
+(define (onTcpMessage message)
+  (display (string-append "message is: " message "\n"))
+)
+
+(define (onKey key scancode action mods)
+  (if (and (= key 257) (= action 1))
+    (send-tcp "test message 1")
+  )
+  (if (and (= key 67) (= action 1))
+    (connect-server (car (list-servers)))
+  )
+)
+
+
