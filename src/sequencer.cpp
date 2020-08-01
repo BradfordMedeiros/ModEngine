9ead4d6181a13d8c00a01fb34dc7cabe4549823a
brad medeiros <bradmedeiros0@gmail.com>
2020-07-31 23:39:49 -0700

    sequencer -> push fns into tracks

--- src/sequencer.cpp
+++ src/sequencer.cpp
@@ -3,6 +3,9 @@
 
 Track createTrack(std::string name, std::vector<std::function<void()>> fns){
   std::vector<std::function<void()>> trackFns;
+  for (auto fn : fns){
+    trackFns.push_back(fn);
+  }
   Track track {
     .name = name,
     .trackFns = trackFns,
@@ -10,6 +13,7 @@ Track createTrack(std::string name, std::vector<std::function<void()>> fns){
   return track;
 }
 void playbackTrack(Track& track){
+  std::cout << "playback track name: " << track.name << std::endl;
   std::cout << "playback track, number fns: " << track.trackFns.size() << std::endl; 
   for (auto trackFn : track.trackFns){
     trackFn();
