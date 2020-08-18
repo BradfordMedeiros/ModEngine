5bd150eb34962b50b185698d0ec6269bd27d205e
brad medeiros <bradmedeiros0@gmail.com>
2020-08-18 01:23:19 -0700

    scripts - toggle up down - get rid of key handling

--- res/scripts/multiplayer/toggle-up-down.scm
+++ res/scripts/multiplayer/toggle-up-down.scm
@@ -7,12 +7,6 @@
   )
 )
 
-(define (onKey key scancode action mods)
-  (if (and (equal? key 75) (equal? action 0))
-    (toggleDoor)
-  )
-)
-
 (define (moveDoor delta)
   (let ((gameobj (lsobj-name "door")))
     (gameobj-setpos-rel! gameobj (list 0 delta 0))
