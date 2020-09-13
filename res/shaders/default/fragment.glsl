6050c57bae31cf61b521c6c7ac4798747f6e4e62
brad medeiros <bradmedeiros0@gmail.com>
2020-09-12 18:25:14 -0700

    fragment shader - delete unused code

--- res/shaders/default/fragment.glsl
+++ res/shaders/default/fragment.glsl
@@ -4,11 +4,6 @@ in vec3 FragPos;
 in vec3 Normal;
 in vec2 TexCoord;
 
-// temporary
-in vec4 glFragPos;
-flat in vec4 overcolor;
-//
-
 out vec4 FragColor;
 
 uniform sampler2D maintexture;
@@ -30,15 +25,11 @@ uniform vec3 lights[MAX_LIGHTS];
 uniform vec3 lightscolor[MAX_LIGHTS];
 uniform vec3 lightsdir[MAX_LIGHTS];
 
-uniform bool hasBones;
-uniform mat4 bones[100];
-
 const float constant = 0.1;
 const float linear = 0.1;
 const float quadratic = 0.0;
 
 const float emissionAmount = 1;
-
 uniform float discardTexAmount;
 
 void main(){
