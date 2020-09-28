d19e28f904ba12069f0b40ef31934019a917a604
brad medeiros <bradmedeiros0@gmail.com>
2020-09-27 17:21:55 -0700

    heightmap normals calculated

--- src/scene/types/heightmap.cpp
+++ src/scene/types/heightmap.cpp
@@ -58,6 +58,7 @@ int indexFromCoords(HeightMapData& heightmap, int vertexX, int vertexY){
 // heightmap can share extra indicies for connected squares (they dont)
 MeshData generateHeightmapMeshdata(HeightMapData& heightmap){
   std::vector<Vertex> vertices;
+  std::vector<int> frequency;
   std::vector<unsigned int> indices;
 
   glm::vec3 position;
@@ -71,17 +72,19 @@ MeshData generateHeightmapMeshdata(HeightMapData& heightmap){
       auto height = heightmap.data[(h * heightmap.width) + w];
       Vertex vertex {
         .position = glm::vec3(w - (heightmap.width / 2.f), height, h - (heightmap.height / 2.f)),
-        .normal = glm::vec3(0.f, 0.f, 0.f), // todo 
+        .normal = glm::vec3(0.f, 1.f, 0.f), // todo 
         .texCoords = glm::vec2(w * 1.f / heightmap.width, h * 1.f / heightmap.height),   // todo 
       };  
       vertices.push_back(vertex);    
+      frequency.push_back(0);
     }
   }
+
   // Remember CCW winding order (for face culling)  -- see ./misc/hmtriangles.png
   for (int h = 1; h < heightmap.height; h++){
     for (int w = 1; w < heightmap.width; w++){
       // first triangle
-      indices.push_back(indexFromCoords(heightmap, w-1, h));
+      indices.push_back(indexFromCoords(heightmap, w - 1, h));
       indices.push_back(indexFromCoords(heightmap, w, h));      
       indices.push_back(indexFromCoords(heightmap, w, h - 1));      
 
@@ -91,6 +94,33 @@ MeshData generateHeightmapMeshdata(HeightMapData& heightmap){
       indices.push_back(indexFromCoords(heightmap, w - 1, h));      
     }
   }
+
+  // every 3 indices make a triangle, take those vertices + cross product, and you have a normal 
+  // add that normal to a list for that indices in a list
+  // then loop over the vertices and divide by how many times that vertex appeared 
+  // (could probably derive frequency w/o memory since all interior have same amount and exterior)
+  for (int i = 0; i < indices.size(); i+=3){ 
+    auto vert0 = indices.at(i);
+    auto vert1 = indices.at(i + 1);
+    auto vert2 = indices.at(i + 2);
+    auto point0 = vertices.at(vert0).position;
+    auto point1 = vertices.at(vert1).position;
+    auto point2 = vertices.at(vert2).position;
+    auto sideOne = point1 - point0; 
+    auto sideTwo = point2 - point0;
+    auto normal = glm::cross(sideOne, sideTwo);     // right hand rule determines if this is up or down 
+    frequency.at(vert0) = frequency.at(vert0) + 1;
+    vertices.at(vert0).normal += normal;
+    frequency.at(vert1) = frequency.at(vert1) + 1;
+    vertices.at(vert1).normal += normal;
+    frequency.at(vert2) = frequency.at(vert2) + 1;
+    vertices.at(vert2).normal += normal;
+  }
+
+  for (int i = 0; i < vertices.size(); i++){
+    vertices.at(i).normal /= ((float)frequency.at(i));
+  }
+
 
 
   BoundInfo boundInfo{};
