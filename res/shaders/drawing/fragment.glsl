4cf1ed650d32d86d47e43429b0b40eca7bc1e2e6
brad medeiros <bradmedeiros0@gmail.com>
2020-10-08 21:34:45 -0700

    fragment shader - tint

--- res/shaders/drawing/fragment.glsl
+++ res/shaders/drawing/fragment.glsl
@@ -3,8 +3,9 @@
 out vec4 FragColor;
 in vec2 TexCoords;
 uniform sampler2D framebufferTexture;
+uniform float opacity;
+uniform vec3 tint;
 
-float opacity = 0.5;
 void main(){
-   FragColor = texture(framebufferTexture, TexCoords) * vec4(1, 1, 1, opacity);
+   FragColor = (vec4(tint.rgb, 1.0) * texture(framebufferTexture, TexCoords)) * vec4(1, 1, 1, opacity);
 }
