f06bce3a1200980cbc787a7c575a6263d84ef134
brad medeiros <bradmedeiros0@gmail.com>
2020-10-09 19:47:47 -0700

    blur shader work

--- res/shaders/blur/fragment.glsl
+++ res/shaders/blur/fragment.glsl
@@ -5,7 +5,7 @@ in vec2 TexCoords;
 uniform sampler2D framebufferTexture;
 
 uniform bool horizontal;
-uniform float weight[5] = float[] (0.227027, 0.1945946, 0.1216216, 0.054054, 0.216216);
+uniform float weight[5] = float[] (0.227027, 0.1945946, 0.1216216, 0.054054, 0.216216); // source learnopengl.com 
 
 void main(){
    vec2 texSize = 1.0 / textureSize(framebufferTexture, 0);
@@ -13,13 +13,13 @@ void main(){
 
    if (horizontal){
       for (int i = 1; i < 5; i++){
-        result += texture(framebufferTexture, TexCoords + vec2(texSize.x * i, 0.0)).rgb * weight[i];
-        result += texture(framebufferTexture, TexCoords - vec2(texSize.x * i, 0.0)).rgb * weight[i];
+        result += weight[i] * texture(framebufferTexture, TexCoords + vec2(texSize.x * i, 0.0)).rgb;
+        result += weight[i] * texture(framebufferTexture, TexCoords - vec2(texSize.x * i, 0.0)).rgb;
       }
     }else{
       for (int i = 1; i < 5; i++){
-        result += texture(framebufferTexture, TexCoords + vec2(0.0, texSize.y * i)).rgb * weight[i];
-        result += texture(framebufferTexture, TexCoords - vec2(0.0, texSize.y * i)).rgb * weight[i];
+        result += weight[i] * texture(framebufferTexture, TexCoords + vec2(0.0, texSize.y * i)).rgb;
+        result += weight[i] * texture(framebufferTexture, TexCoords - vec2(0.0, texSize.y * i)).rgb;
       }
     }
 
--- res/shaders/framebuffer/fragment.glsl
+++ res/shaders/framebuffer/fragment.glsl
@@ -1,5 +1,4 @@
 #version 330 core 
-
 out vec4 FragColor;
 in vec2 TexCoords;
 uniform sampler2D framebufferTexture;
