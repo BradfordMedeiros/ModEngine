9c3f89aded9a8080459cab2b61a379b885d89cbb
brad medeiros <bradmedeiros0@gmail.com>
2020-10-25 22:49:29 -0700

    navmesh basic defn

--- src/scene/object_types.cpp
+++ src/scene/object_types.cpp
@@ -450,6 +450,14 @@ void renderObject(
     drawMesh(heightmapObj -> mesh, shaderProgram, heightmapObj -> texture.textureOverloadId);   
   }
 
+  auto navmeshObj = std::get_if<GameObjectNavmesh>(&toRender);
+  if (navmeshObj != NULL && showDebug){
+    glUniform1i(glGetUniformLocation(shaderProgram, "hasBones"), nodeMesh.bones.size() > 0);
+    glUniform2fv(glGetUniformLocation(shaderProgram, "textureOffset"), 1, glm::value_ptr(glm::vec2(0.f, 0.f)));  
+    glUniform2fv(glGetUniformLocation(shaderProgram, "textureTiling"), 1, glm::value_ptr(glm::vec2(1.f, 1.f)));
+    drawMesh(nodeMesh, shaderProgram);
+  }
+
   auto uiObj = std::get_if<GameObjectUIButton>(&toRender);
   if (uiObj != NULL){
     glUniform1i(glGetUniformLocation(shaderProgram, "hasBones"), nodeMesh.bones.size() > 0);
