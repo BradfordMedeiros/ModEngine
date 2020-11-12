6a026471796267b31ca815cc9d15003a91865129
brad medeiros <bradmedeiros0@gmail.com>
2020-11-11 18:45:41 -0800

    video -> wip

--- src/video.cpp
+++ src/video.cpp
@@ -17,9 +17,43 @@ void testvideo(){
     assert(false);
   }
 
+  auto streamOpenValue = avformat_find_stream_info(formatContext, NULL);
+  if (streamOpenValue < 0){
+    std::cout << "ERROR: could not find stream info" << std::endl;
+    assert(false);
+  }
+
+  auto numStreams = formatContext -> nb_streams;
+
   std::cout << "video: " << videopath << " is: " << formatContext -> duration << std::endl;
-/*
+  std::cout << "number video streams: " << numStreams << std::endl;
+
+  for (int i = 0; i < numStreams; i++){
+    std::cout << "stream number: " << i << std::endl;
+    AVCodecParameters* codecParameters = formatContext -> streams[i] -> codecpar;
+    std::cout << "bit rate: " << codecParameters -> bit_rate << std::endl;
+    std::cout << "width: " << codecParameters -> width << std::endl;
+    std::cout << "height: " << codecParameters -> height << std::endl;
+
+    AVMediaType mediaType = codecParameters -> codec_type;
+    if (mediaType == AVMEDIA_TYPE_VIDEO){
+      std::cout << "media type: video" << std::endl;
+    }else if (mediaType == AVMEDIA_TYPE_AUDIO){ 
+      std::cout << "media type: audio" << std::endl;
+    }else{
+      std::cout << "ERROR: VIDEO: UNHANDLED MEDIA TYPE" << std::endl;
+      assert(false);
+    }
+
+    AVCodec* localCodec = avcodec_find_decoder(codecParameters -> codec_id);
+    assert(localCodec != NULL);
+    std::cout << "INFO: video: found decoder for codec" << std::endl;
+
+    std::cout << std::endl;
+  }
+/*  
   avformat_close_input(&formatContext);*/
 
+  assert(false);
 }
 
