bb2891f6a5a2a686d1559225d56d14bb36d33f70
brad medeiros <bradmedeiros0@gmail.com>
2020-11-15 14:57:26 -0800

    include video type files

---
+++ src/scene/types/video.h
@@ -0,0 +1,35 @@
+#ifndef MOD_VIDEO
+#define MOD_VIDEO
+
+#include <iostream>
+#include <assert.h>
+#include <functional>
+
+extern "C" {
+  #include <libavcodec/avcodec.h>
+  #include <libavformat/avformat.h>
+}
+
+struct StreamIndexs {
+  int video;
+  int audio;
+};
+
+struct StreamCodecs {
+  AVCodecContext *videoCodec;
+  AVCodecContext *audioCodec;
+};
+
+struct VideoContent {
+  AVFormatContext* formatContext;
+  AVFrame* avFrame;
+  AVPacket *avPacket;
+  StreamIndexs streamIndexs;
+  StreamCodecs codecs;
+};
+
+VideoContent loadVideo();
+AVFrame* nextFrame(VideoContent& content);
+void freeVideoContent(VideoContent& content);
+
+#endif
