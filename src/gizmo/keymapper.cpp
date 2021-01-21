1a857b849d1b1d22be1d1c538cb6936a44f3f404
brad medeiros <bradmedeiros0@gmail.com>
2021-01-20 20:01:21 -0800

    keymapper work

--- src/gizmo/keymapper.cpp
+++ src/gizmo/keymapper.cpp
@@ -17,11 +17,33 @@ bool isNamedValue(std::string value){
 struct NamedValue {
   std::string command;
   int index;
+  float deadzonemin;
+  float deadzonemax;
+  float trigger;
 };
 NamedValue parseNamedValue(std::string value){
+  auto parts = filterWhitespace(split(value, ':'));
+  auto command = parts.at(0);
+  auto index = std::atoi(parts.at(1).c_str());
+
+  float deadzonemin = 0.f;
+  float deadzonemax = 0.f;
+  float trigger = 1.f;
+
+  if (command == "axis-deadzone"){
+    deadzonemin = std::atof(parts.at(2).c_str());
+    deadzonemax = std::atof(parts.at(3).c_str());
+  }
+  if (command == "axis-trigger"){
+    trigger = std::atof(parts.at(2).c_str());
+  }
+
   return NamedValue {
-    .command = "button", 
-    .index = 0,
+    .command = command, 
+    .index = index,
+    .deadzonemin = deadzonemin,
+    .deadzonemax = deadzonemax,
+    .trigger = trigger,
   };
 }
 
@@ -49,64 +71,75 @@ KeyRemapper readMapping(std::string filemapping){
 
   std::vector<KeyMapping> mapping;
   std::vector<KeyMapping> buttonMappings;                
+  std::vector<KeyAxisConfiguration> axisConfigurations; 
   std::string keyMapperContent = loadFile(filemapping);
 
   auto mappedLines = filterWhitespace(filterComments(split(keyMapperContent, '\n')));
   for (auto line : mappedLines){
     auto expression = split(line, '>');
-    if (expression.size() != 2){
-      std::cout << "INFO: key mapping " << filemapping << " is invalid\n" << line << std::endl;
-      exit(1);
-    }
 
-    auto expr1 = trim(expression.at(0));
-    auto expr2 = trim(expression.at(1));
-    auto keyType = getKeyValueType(expr1);
-    auto keyType2 = getKeyValueType(expr2);
+    if (expression.size() == 1){
+      auto data = trim(expression.at(0));
+      auto keyType = getKeyValueType(data);
+      if (keyType == KEY_VALUE_NAMED){
+        auto value = parseNamedValue(data);
+        if (value.command == "axis-deadzone"){
+          axisConfigurations.push_back(KeyAxisConfiguration{
+            .index = value.index,
+            .deadzonemin = value.deadzonemin,
+            .deadzonemax = value.deadzonemax,
+          });
+        }else{
+          std::cout << "invalid command type" << std::endl;
+          assert(false);
+        }
+      }else{
+        std::cout << "invalid value -- " <<  data << std::endl;
+        assert(false);
+      }
+    }else if (expression.size() == 2){
+      auto expr1 = trim(expression.at(0));
+      auto expr2 = trim(expression.at(1));
+      auto keyType = getKeyValueType(expr1);
+      auto keyType2 = getKeyValueType(expr2);
 
-    if ((keyType == KEY_VALUE_ASCII  || keyType == KEY_VALUE_CHAR) && (keyType2 == KEY_VALUE_ASCII || keyType2 == KEY_VALUE_CHAR)){
-      int fromChar = keyType == KEY_VALUE_ASCII ?  parseAsciiValue(expr1) : parseCharValue(expr1);
-      int secondPart = keyType2 == KEY_VALUE_ASCII ?  parseAsciiValue(expr2) : parseCharValue(expr2);
-      assert(fromChar >= 0);
-      assert(secondPart >= 0);
-      mapping.push_back(KeyMapping{
-        .sourceKey = fromChar,
-        .destinationKey = secondPart,
-      });
-    }else if (keyType == KEY_VALUE_NAMED && (keyType2 == KEY_VALUE_ASCII || keyType2 == KEY_VALUE_CHAR)){
-      std::cout << "Keymapping: not yet implemented" << std::endl;
-      auto namedValue = parseNamedValue(expr1);
-      int secondPart = keyType2 == KEY_VALUE_ASCII ?  parseAsciiValue(expr2) : parseCharValue(expr2);
-      if (namedValue.command == "button"){
-        buttonMappings.push_back(KeyMapping {
-          .sourceKey = namedValue.index,
+      if ((keyType == KEY_VALUE_ASCII  || keyType == KEY_VALUE_CHAR) && (keyType2 == KEY_VALUE_ASCII || keyType2 == KEY_VALUE_CHAR)){
+        int fromChar = keyType == KEY_VALUE_ASCII ?  parseAsciiValue(expr1) : parseCharValue(expr1);
+        int secondPart = keyType2 == KEY_VALUE_ASCII ?  parseAsciiValue(expr2) : parseCharValue(expr2);
+        assert(fromChar >= 0);
+        assert(secondPart >= 0);
+        mapping.push_back(KeyMapping{
+          .sourceKey = fromChar,
           .destinationKey = secondPart,
         });
+      }else if (keyType == KEY_VALUE_NAMED && (keyType2 == KEY_VALUE_ASCII || keyType2 == KEY_VALUE_CHAR)){
+        std::cout << "Keymapping: not yet implemented" << std::endl;
+        auto namedValue = parseNamedValue(expr1);
+        int secondPart = keyType2 == KEY_VALUE_ASCII ?  parseAsciiValue(expr2) : parseCharValue(expr2);
+        if (namedValue.command == "button"){
+          buttonMappings.push_back(KeyMapping {
+            .sourceKey = namedValue.index,
+            .destinationKey = secondPart,
+          });
+        }else if (namedValue.command == "axis-trigger"){
+          axisConfigurations.push_back(
+            KeyAxisConfiguration{
+              .index = namedValue.index,
+              .shouldMapKey = true,
+              .amount = namedValue.trigger,
+              .destinationKey = secondPart,
+            }
+          );
+        }
+      }else{
+        std::cout << "Keymapping: combination not supported" << std::endl;
       }
-    }else{
-      std::cout << "Keymapping: combination not supported" << std::endl;
-    }
 
+      }else{
+        std::cout << "INFO: key mapping " << filemapping << " is invalid\n" << line << std::endl;
+        exit(1);
+      }
   }
-
-  std::vector<KeyAxisConfiguration> axisConfigurations; 
-  axisConfigurations.push_back(KeyAxisConfiguration{
-    .index = 1,
-    .deadzonemin = -0.1f,
-    .deadzonemax = 0.1f,
-  });
-  axisConfigurations.push_back(KeyAxisConfiguration{
-    .index = 0,
-    .deadzonemin = -0.1f,
-    .deadzonemax = 0.1f,
-  });
-
-  axisConfigurations.push_back(KeyAxisConfiguration{
-    .index = 5,
-    .shouldMapKey = true,
-    .amount = 0.7,
-    .destinationKey = 68,
-  });
 
   KeyRemapper remapper {
     .mapping = mapping,
