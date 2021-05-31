07f1c710da177dc88ea80dea816dbda48fb167af
brad medeiros <bradmedeiros0@gmail.com>
2021-05-31 16:01:24 -0700

    features -> spriteanimation

--- res/scenes/features/textures/spriteanimation.scm
+++ res/scenes/features/textures/spriteanimation.scm
@@ -1,3 +1,23 @@
+(set! mainobj (lsobj-name "1/Plane"))
+
+(define lasttime (time-seconds))
+(define (time-elapsed amount) 
+  (> (- (time-seconds) lasttime) amount)
+)
+
+(define xpos 0)
 (define (onFrame)
-  (display "hello world\n")
+  (if (time-elapsed 0.1)
+    (begin
+      (set! lasttime (time-seconds))
+      (set! xpos (+ xpos 0.125))
+      (if (> xpos 1) (set! xpos 0))
+      (gameobj-setattr! 
+        mainobj
+        (list
+          (list "textureoffset" (string-join (list (number->string xpos) "0") " "))
+        )
+      )
+    )
+  ) 
 )
