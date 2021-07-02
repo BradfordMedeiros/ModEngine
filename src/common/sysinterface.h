bc199de16701c64d9187eb6420693ce4300d2f3f
brad medeiros <bradmedeiros0@gmail.com>
2021-07-01 22:16:18 -0700

    scheme -> load script with id -> include sysinterface.h

--- src/common/sysinterface.h
+++ src/common/sysinterface.h
@@ -5,7 +5,7 @@
 #include "./util.h"
 
 struct SysInterface {
-  std::function<void(std::string, objid)> loadScript;
+  std::function<void(std::string, objid, objid)> loadScript;
   std::function<void(std::string, objid)> unloadScript;
   std::function<void(objid)> stopAnimation;
   std::function<float()> getCurrentTime;
