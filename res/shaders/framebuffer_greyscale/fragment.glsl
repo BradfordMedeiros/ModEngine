dc31a40204d1ba26933f41d60c317532ab763877
brad medeiros <bradmedeiros0@gmail.com>
2021-07-27 19:06:40 -0700

    update greyscale frambuffer to take into account bloom

--- res/shaders/framebuffer_greyscale/fragment.glsl
+++ res/shaders/framebuffer_greyscale/fragment.glsl
@@ -3,9 +3,18 @@
 out vec4 FragColor;
 in vec2 TexCoords;
 uniform sampler2D framebufferTexture;
+uniform sampler2D bloomTexture;
+
+uniform bool enableBloom;
+uniform float bloomAmount;
 
 void main(){
-   vec4 color = texture(framebufferTexture, TexCoords);
-   float avgColor = (color.r + color.g + color.b) / 3;
-   FragColor = vec4(avgColor, avgColor, avgColor, color.a);
+  vec4 color;
+  if (enableBloom){
+    color = texture(framebufferTexture, TexCoords) + (bloomAmount * texture(bloomTexture, TexCoords));
+  }else{
+    color = texture(framebufferTexture, TexCoords);
+  }
+  float avgColor = (color.r + color.g + color.b) / 3;
+  FragColor = vec4(avgColor, avgColor, avgColor, color.a);
 }
