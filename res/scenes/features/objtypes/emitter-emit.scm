add62e53a3b8096c1896be73611c09757911667d
brad medeiros <bradmedeiros0@gmail.com>
2021-09-28 00:10:26 -0700

    scenes -> features -> emitter

--- res/scenes/features/objtypes/emitter-emit.scm
+++ res/scenes/features/objtypes/emitter-emit.scm
@@ -2,7 +2,7 @@
 
 (define emitterOn #t)
 (define (onKey key scancode action mods)
-  (if (and (= key 46) (= action 1))
+  (if (and (= key 332) (= action 1))  ; *
     (begin
       (set! emitterOn (not emitterOn))
       (display "setting emitter state: ")
@@ -15,8 +15,11 @@
       )
     )
   )
-  (if (and (= key 47) (= action 1))
+)
+
+(define (onMouse button action mods)
+  (format #t "button is: ~a ~a\n" button action)
+  (if (and (= button 0) (= action 1))
     (emit (gameobj-id mainobj))
   )
 )
-
