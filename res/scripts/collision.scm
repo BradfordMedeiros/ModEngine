70a47896924862aace9132da81ee9fe571959504
brad medeiros <bradmedeiros0@gmail.com>
2021-09-29 19:26:21 -0700

    collision script update

--- res/scripts/collision.scm
+++ res/scripts/collision.scm
@@ -1,19 +1,15 @@
 
-(define last-ray-from-1 '(1 0 0))
-(define last-ray-to-1 '(1 10 0))
+(define last-ray-from-1 (list 1 0 0))
+(define last-ray-to-1 (list 1 10 0))
 
-(define (onCollideEnter obj1 obj2 hitpoint normal)
+(define (onGlobalCollideEnter obj1 obj2 hitpoint normal normal2)
+  (display "on collide enter\n")
+  (format #t "obj1 id: ~a, obj2 id: ~a, hitpoint: ~a, normal: ~a, normal2: ~a\n" (gameobj-name obj1) (gameobj-name obj2) hitpoint normal normal2)
   (set! last-ray-from-1 hitpoint)
-  (set! last-ray-to-1 (move-relative hitpoint normal 10))
-  (display "obj1 id: ")
-  (display (gameobj-name obj1))
-  (display "\n")
-  (display "obj2 id: ")
-  (display (gameobj-name obj2))
-  (display "\n")
+  (set! last-ray-to-1 (move-relative hitpoint (orientation-from-pos (list 0 0 0) normal) 10))
 )
 
-(define (onCollideExit obj1 obj2)
+(define (onGlobalCollideExit obj1 obj2)
   (display "on collide exit\n")
 )
 
