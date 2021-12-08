5c85a0c9e9b041dacb21bd9c9d71e981243e9f1d
brad medeiros <bradmedeiros0@gmail.com>
2021-12-07 18:43:06 -0800

    bloom -> don't disable depth test, depth texture -> false

--- src/main.cpp
+++ src/main.cpp
@@ -1385,13 +1385,14 @@ int main(int argc, char* argv[]){
     PROFILE("BLOOM-RENDERING",
       setActiveDepthTexture(1);
       glUseProgram(blurProgram);
+      glUniform1i(glGetUniformLocation(blurProgram, "useDepthTexture"), false);
       glUniform1i(glGetUniformLocation(blurProgram, "firstpass"), true);
       glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, framebufferTexture3, 0);
       glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT1, GL_TEXTURE_2D, framebufferTexture3, 0);
 
       glClearColor(0.0f, 1.0f, 0.0f, 1.0f);
       glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-      glDisable(GL_DEPTH_TEST | GL_STENCIL_TEST);
+      glDisable(GL_STENCIL_TEST);
 
       glBindTexture(GL_TEXTURE_2D, framebufferTexture2);
       glBindVertexArray(quadVAO);
