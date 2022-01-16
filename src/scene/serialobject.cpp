2a5e9d4796ec3b6115cac58c2dcd4ac74661e471
brad medeiros <bradmedeiros0@gmail.com>
2022-01-15 18:42:58 -0800

    cleanup -> get rid of serialobject and set on gameobject directly

--- src/scene/serialobject.cpp
+++ src/scene/serialobject.cpp
@@ -1,21 +1,5 @@
 #include "./serialobject.h"
 
-struct SerializationObject {
-  bool hasId;
-  int id;
-  glm::vec3 position;
-  glm::vec3 scale;
-  glm::quat rotation;
-  std::vector<std::string> children;
-  physicsOpts physics;
-  std::string lookat;
-  std::string layer;
-  std::string script;
-  std::string fragshader;
-  bool netsynchronize;
-  GameobjAttributes additionalFields;
-};
-
 void safeVecSet(glm::vec3* value, const char* key, GameobjAttributes& attributes, glm::vec3* defaultValue){
   if (attributes.vecAttributes.find(key) != attributes.vecAttributes.end()){
     *value = attributes.vecAttributes.at(key);
@@ -36,27 +20,31 @@ void safeStringSet(std::string* value, const char* key, GameobjAttributes& attri
   }
 }
 
-void setSerialObjFromAttr(SerializationObject& object, GameobjAttributes& attributes){
+void setSerialObjFromAttr(GameObject& object, GameobjAttributes& attributes){
+  if (attributes.stringAttributes.find("id") != attributes.stringAttributes.end()){
+    object.id = std::atoi(attributes.stringAttributes.at("id").c_str());
+  }
+  
   auto identityVec = glm::vec3(1.f, 1.f, 1.f);
   auto zeroVec = glm::vec3(0.f, 0.f, 0.f);
   auto defaultGravity = glm::vec3(0.f, -9.81f, 0.f);
 
-  safeVecSet(&object.position, "position", attributes, &zeroVec);
-  safeVecSet(&object.scale, "scale", attributes, &identityVec);
-  safeVecSet(&object.physics.angularFactor, "physics_angle", attributes, &identityVec);
-  safeVecSet(&object.physics.linearFactor, "physics_linear", attributes, &identityVec);
-  safeVecSet(&object.physics.gravity, "physics_gravity", attributes, &defaultGravity);
-  safeVecSet(&object.physics.velocity, "physics_velocity", attributes, &zeroVec);
-  safeVecSet(&object.physics.angularVelocity, "physics_avelocity", attributes, &zeroVec);
+  safeVecSet(&object.transformation.position, "position", attributes, &zeroVec);
+  safeVecSet(&object.transformation.scale, "scale", attributes, &identityVec);
+  safeVecSet(&object.physicsOptions.angularFactor, "physics_angle", attributes, &identityVec);
+  safeVecSet(&object.physicsOptions.linearFactor, "physics_linear", attributes, &identityVec);
+  safeVecSet(&object.physicsOptions.gravity, "physics_gravity", attributes, &defaultGravity);
+  safeVecSet(&object.physicsOptions.velocity, "physics_velocity", attributes, &zeroVec);
+  safeVecSet(&object.physicsOptions.angularVelocity, "physics_avelocity", attributes, &zeroVec);
 
   auto oneValue = 1.f;
   auto negOneValue = -1.f;
   auto zeroValue = 0.f;
-  safeFloatSet(&object.physics.friction, "physics_friction", attributes, &oneValue);
-  safeFloatSet(&object.physics.restitution, "physics_restitution", attributes, &zeroValue);
-  safeFloatSet(&object.physics.mass, "physics_mass", attributes, &oneValue);
-  safeFloatSet(&object.physics.maxspeed, "physics_maxspeed", attributes, &negOneValue); // -1 ?  does sign matter? 
-  safeFloatSet(&object.physics.layer, "physics_layer", attributes, &zeroValue);
+  safeFloatSet(&object.physicsOptions.friction, "physics_friction", attributes, &oneValue);
+  safeFloatSet(&object.physicsOptions.restitution, "physics_restitution", attributes, &zeroValue);
+  safeFloatSet(&object.physicsOptions.mass, "physics_mass", attributes, &oneValue);
+  safeFloatSet(&object.physicsOptions.maxspeed, "physics_maxspeed", attributes, &negOneValue); // -1 ?  does sign matter? 
+  safeFloatSet(&object.physicsOptions.layer, "physics_layer", attributes, &zeroValue);
 
   safeStringSet(&object.lookat, "lookat", attributes);
   safeStringSet(&object.script, "script", attributes);
@@ -65,42 +53,42 @@ void setSerialObjFromAttr(SerializationObject& object, GameobjAttributes& attrib
 
   // old default in getDefaultPhysics was false
   if (attributes.stringAttributes.find("physics") != attributes.stringAttributes.end()){
-    object.physics.enabled = attributes.stringAttributes.at("physics") == "enabled";
+    object.physicsOptions.enabled = attributes.stringAttributes.at("physics") == "enabled";
   }else{
-    object.physics.enabled = false;
+    object.physicsOptions.enabled = false;
   }
 
   // old default in getDefaultPhysics was false
   if (attributes.stringAttributes.find("physics_collision") != attributes.stringAttributes.end()){
-    object.physics.hasCollisions = !(attributes.stringAttributes.at("physics_collision") == "nocollide");
+    object.physicsOptions.hasCollisions = !(attributes.stringAttributes.at("physics_collision") == "nocollide");
   }else{
-    object.physics.hasCollisions = true;
+    object.physicsOptions.hasCollisions = true;
   }
 
   if (attributes.stringAttributes.find("physics_type") != attributes.stringAttributes.end()){
-    object.physics.isStatic = attributes.stringAttributes.at("physics_type") != "dynamic";
+    object.physicsOptions.isStatic = attributes.stringAttributes.at("physics_type") != "dynamic";
   }else{
-    object.physics.isStatic = true;
+    object.physicsOptions.isStatic = true;
   }
 
   if (attributes.stringAttributes.find("physics_shape") != attributes.stringAttributes.end()){
     auto value = attributes.stringAttributes.at("physics_shape");
     if (value == "shape_sphere"){
-      object.physics.shape = SPHERE;
+      object.physicsOptions.shape = SPHERE;
     }else if (value == "shape_box"){
-      object.physics.shape = BOX;
+      object.physicsOptions.shape = BOX;
     }else if (value == "shape_capsule"){
-      object.physics.shape = CAPSULE;
+      object.physicsOptions.shape = CAPSULE;
     }else if (value == "shape_cylinder"){
-      object.physics.shape = CYLINDER;
+      object.physicsOptions.shape = CYLINDER;
     }else if (value == "shape_hull"){
-      object.physics.shape = CONVEXHULL;
+      object.physicsOptions.shape = CONVEXHULL;
     }else if (value == "shape_auto"){
-      object.physics.shape = AUTOSHAPE;
+      object.physicsOptions.shape = AUTOSHAPE;
     }else if (value == "shape_exact"){
-      object.physics.shape = SHAPE_EXACT;
+      object.physicsOptions.shape = SHAPE_EXACT;
     }else{
-      object.physics.shape = AUTOSHAPE;
+      object.physicsOptions.shape = AUTOSHAPE;
     }
   }
 
@@ -108,43 +96,16 @@ void setSerialObjFromAttr(SerializationObject& object, GameobjAttributes& attrib
     object.netsynchronize = attributes.stringAttributes.at("net") == "sync";
   }
 
-  if (attributes.stringAttributes.find("id") != attributes.stringAttributes.end()){
-    object.id = std::atoi(attributes.stringAttributes.at("id").c_str());
-    object.hasId = true;
-  }else{
-    object.id  = -1;
-    object.hasId = false;
-  }
-
   if (attributes.stringAttributes.find("rotation") != attributes.stringAttributes.end()){
-    object.rotation = parseQuat(attributes.stringAttributes.at("rotation"));
+    object.transformation.rotation = parseQuat(attributes.stringAttributes.at("rotation"));
   }else{
-    object.rotation = glm::identity<glm::quat>();
+    object.transformation.rotation = glm::identity<glm::quat>();
   }
-  object.additionalFields = attributes;
-  object.children = attributes.children;
 }
 
 GameObject gameObjectFromFields(std::string name, objid id, GameobjAttributes attributes){
-  SerializationObject serialObj{};
-  setSerialObjFromAttr(serialObj, attributes);
-
-  GameObject gameObject = {
-    .id = id,
-    .name = name,
-    .transformation = Transformation {
-      .position = serialObj.position,
-      .scale = serialObj.scale,
-      .rotation = serialObj.rotation,
-    },
-    .physicsOptions = serialObj.physics,
-    .lookat =  serialObj.lookat, 
-    .layer = serialObj.layer,
-    .script = serialObj.script,
-    .fragshader = serialObj.fragshader,
-    .netsynchronize = serialObj.netsynchronize,
-  };
-
+  GameObject gameObject = { .id = id, .name = name };
+  setSerialObjFromAttr(gameObject, attributes);
   return gameObject;
 }
 
