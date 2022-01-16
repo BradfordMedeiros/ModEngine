7708ffa763c182fcb6fc78f7d2682025e871f68d
brad medeiros <bradmedeiros0@gmail.com>
2022-01-16 00:33:43 -0800

    -

--- res/scenes/features/animation/animate.scm
+++ res/scenes/features/animation/animate.scm
@@ -1,7 +1,6 @@
 
+(define (animation-name) (cadr (assoc "animation" (gameobj-attr mainobj))))
 (define (onKey key scancode action mods)
-  (if (equal? key 257)
-    (gameobj-playanimation mainobj (string-append (gameobj-name mainobj)))
-  )
+  (if (equal? key 257) (gameobj-playanimation mainobj (animation-name)))
 )
 
