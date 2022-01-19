f3f113151e5af641fbd348ee6f9839369cb8d85a
brad medeiros <bradmedeiros0@gmail.com>
2022-01-18 19:16:20 -0800

    -

--- res/scenes/features/animation/animate.scm
+++ res/scenes/features/animation/animate.scm
@@ -1,6 +1,8 @@
 
 (define (animation-name) (cadr (assoc "animation" (gameobj-attr mainobj))))
 (define (onKey key scancode action mods)
-  (if (equal? key 257) (gameobj-playanimation mainobj (animation-name)))
+  (define animationname (animation-name))
+  (if (equal? key 257) (gameobj-playanimation mainobj animationname))
+  (format #t "playing animation ~a for model  name ~a" animationname (gameobj-name mainobj))
 )
 
