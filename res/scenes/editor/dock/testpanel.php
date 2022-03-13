fd79591e861a95eaf69ef134c6250ad2d798541b
brad medeiros <bradmedeiros0@gmail.com>
2022-03-12 23:40:31 -0800

    ui -> editor php -> dock example

---
+++ res/scenes/editor/dock/testpanel.php
@@ -0,0 +1,14 @@
+
+(test_panel:position:-0.8 0.85 -1  # 0.42, 0.97 is kind of arbitary, needs to be aligned from the left/top.  The actual width is measured and a function of text size and button size, really want layout alignment stuff
+
+(test_panel:layer:basicui
+(test_panel:type:horizontal
+(test_panel:backpanel:true
+(test_panel:tint:1.0 0.1 0.1
+(test_panel:margin:0.02
+(test_panel:spacing:0.02
+(test_panel:elements:)test_panel_placeholder
+
+)test_panel_placeholder:value:this is a dockpanel
+)test_panel_placeholder:layer:basicui
+)test_panel_placeholder:scale:0.01 0.01 0.01
