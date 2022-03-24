11266b02af0e0120244ad2a833ad8cdd22c2c3bb
brad medeiros <bradmedeiros0@gmail.com>
2022-03-24 00:55:40 -0700

    scenes -> features -> texteditor

--- res/scenes/features/uicontrols/texteditor.scm
+++ res/scenes/features/uicontrols/texteditor.scm
@@ -16,12 +16,28 @@
     (begin
       (if (= key 259)
         (set! currentText (substring currentText 0 (lessIndex)))
-        (begin
-          (format #t "key is ~a ~a\n" key (string (integer->char key)))
-          (set! currentText (string-append currentText (string (integer->char key))))
+        (if (= key 257)
+          (set! currentText (string-append currentText "\n"))
+          (begin
+            (format #t "key is ~a ~a\n" key (string (integer->char key)))
+            (set! currentText (string-append currentText (string (integer->char key))))
+          )
         )
+
+      )
+      (if (= key 345)
+        (write-tofile)
       )
       (updateText currentText)
     )
+  )
+)
+
+
+(define (write-tofile)
+  (let ((output-port (open-file "modtext.txt" "w")))
+    (display currentText output-port)
+    (newline output-port)
+    (close output-port)
   )
 )
