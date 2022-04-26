6fda1c3811d9db9ad81aba4056fa60682b51f4f7
brad medeiros <bradmedeiros0@gmail.com>
2022-04-25 22:31:06 -0700

    editor -> wip ->  popover actions

--- res/scenes/editor/editor.scm
+++ res/scenes/editor/editor.scm
@@ -1,19 +1,39 @@
 (define uilist 
   (list
-    (list "file" (list "open" "save" "load" "quit"))
-    (list "misc" (list "fullscreen"))
+    (list "file" (reverse (list "open" "save" "load" "quit")))
+    (list "misc" (reverse (list "fullscreen")))
   )
 )
+
+(define nameAction (list
+  (list "open" (lambda () (format #t "open placeholder\n")) )
+  (list "save" (lambda () ( format #t "save placeholder\n")) )
+  (list "load" (lambda () ( format #t "load placeholder\n")) )
+  (list "quit" (lambda () (exit 0)) )
+  (list "fullscreen" (lambda () 
+      (format #t "toggling fullscreen\n")
+      (set-wstate (list
+        (list "rendering" "fullscreen" "true") ; doesn't actually toggle since fullscreen state never updates to match internal with set-wstate
+      ))
+    )
+  )
+))
+ 
 (define menuOptions (map car uilist))
 
-(define (textvalue name content)
+(define (textvalue name content action)
   (list name
     (list 
       (list name "layer" "basicui")
       (list name "scale" "0.005 0.015 0.005")
       (list name "value" content)
+      (list name "popaction" action)
     )
   )
+)
+(define (popoverAction action)
+  (define mappedAction (assoc action nameAction))
+  (if mappedAction ((cadr mappedAction)))
 )
 (define (popover-options menuOptions)
   (define index -1)
@@ -22,7 +42,7 @@
     (set! index (+ index 1))
     (string-append ")text_" (number->string index))
   )
-  (define (create-value text) (textvalue (nextName) text))
+  (define (create-value text) (textvalue (nextName) text text))
 
   (define val (map create-value menuOptions))
   (define itemnames (map car val))
@@ -46,13 +66,21 @@
 )
 
 
+(define (isPopoverElement name) (if (< (string-length name) 4) #f (equal? (substring name 0 6) ")text_")) )
+
 (define (onObjSelected gameobj color)
-  (define popoptionPair (assoc "popoption" (gameobj-attr gameobj)))
+  (define objattrs (gameobj-attr gameobj))
+  (define popoptionPair (assoc "popoption" objattrs))
+  (define popactionPair (assoc "popaction" objattrs))
   (define popoption (if popoptionPair (cadr popoptionPair) ""))
   (define isInList (not (equal? #f (member popoption menuOptions))))
+  (define elementName (gameobj-name gameobj))
   (format #t "popoption: ~a\n" popoption)
   (if isInList
     (change-popover popoption)
+  )
+  (if (and (isPopoverElement elementName) popactionPair) 
+    (popoverAction (cadr popactionPair))
   )
 )
 
