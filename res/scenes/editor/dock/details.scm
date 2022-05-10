97c9218c6cee507229c9e8ec1fc320340398ccbe
brad medeiros <bradmedeiros0@gmail.com>
2022-05-10 09:08:07 -0700

    editor -> blue siding

--- res/scenes/editor/dock/details.scm
+++ res/scenes/editor/dock/details.scm
@@ -3,3 +3,7 @@
     (sendnotify "dock-self-remove" "true")
   )
 )
+
+;; todo remove - no items in this layout, should require this 
+(enforce-layout (gameobj-id (lsobj-name "(banner_title_background_right" )))
+(enforce-layout (gameobj-id (lsobj-name "(banner_title_background_left" )))
