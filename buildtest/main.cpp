9433837f63cfa4261f1cb1a7b0aa3184c17ef021
Samantha <samanthawang318@gmail.com>
2022-05-25 10:10:03 -0700

    build test -> conditional include pattern defined

--- buildtest/main.cpp
+++ buildtest/main.cpp
@@ -1,5 +1,54 @@
 #include <iostream>
 
+#ifdef INCLUDE_GLFW
+  #include <GLFW/glfw3.h>
+
+  void glfwTest(){
+  	std::cout << "placeholder glfw test" << std::endl;
+  }
+#endif
+
+#ifdef INCLUDE_GLAD
+#endif
+
+#ifdef INCLUDE_GLM
+  #include <glm/glm.hpp>
+  void glmTest(){
+  	glm::vec3 one(1.f, 2.f, 3.f);
+  	glm::vec3 two(2.4f, 5.3, 3.5);
+  	assert((one + two).x > 2); 
+  	std::cout << "Some GLM Includes Verified" << std::endl;
+  } 
+#endif
+
+#ifdef INCLUDE_STB
+
+#endif
+
+#ifdef INCLUDE_OPENAL
+#endif
+
+#ifdef INCLUDE_CXXOPTS
+
+#endif
+
+#ifdef INCLUDE_ASSIMP
+
+#endif
+
+#ifdef INCLUDE_BULLET
+
+#endif
+
 int main(){
+	#ifdef INCLUDE_GLFW
+		glfwTest();
+	#endif
+
+	#ifdef INCLUDE_GLM
+		glmTest();
+	#endif
+
+
 	std::cout << "Build test runs!" << std::endl;
 }
