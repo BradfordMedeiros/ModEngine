0dbd8e66128f6b8bd170a2128e287d8681905906
Samantha <samanthawang318@gmail.com>
2022-05-25 12:38:21 -0700

    buildtest -> structure for conditional tests

--- buildtest/main.cpp
+++ buildtest/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <assert.h>
 
 #ifdef INCLUDE_GLFW
   #include <GLFW/glfw3.h>
@@ -9,6 +10,9 @@
 #endif
 
 #ifdef INCLUDE_GLAD
+  void gladTest(){
+
+  }
 #endif
 
 #ifdef INCLUDE_GLM
@@ -17,36 +21,81 @@
   	glm::vec3 one(1.f, 2.f, 3.f);
   	glm::vec3 two(2.4f, 5.3, 3.5);
   	assert((one + two).x > 2); 
-  	std::cout << "Some GLM Includes Verified" << std::endl;
+  	std::cout << "GLM Verified" << std::endl;
   } 
 #endif
 
 #ifdef INCLUDE_STB
+  #include <stb_image.h>
+  #include <stb_image_write.h>
+  void stbImageTest(){
 
+  }
 #endif
 
 #ifdef INCLUDE_OPENAL
+  void openAlTest(){
+
+  }
 #endif
 
 #ifdef INCLUDE_CXXOPTS
-
+  #include <cxxopts.hpp>
+  void cxxoptsTest(int argc, char* argv[]){
+  	cxxopts::Options cxxoption("ModEngine", "ModEngine is a game engine for hardcore fps");
+  	cxxoption.add_options()
+   	  ("q,testarg", "Hide the window of the game engine", cxxopts::value<bool>()->default_value("true"))
+    ;        
+  	const auto result = cxxoption.parse(argc, argv);
+  	auto testArg = result["testarg"].as<bool>();
+  	assert(testArg);
+   	std::cout << "CXXOpts Verified" << std::endl;
+  }
 #endif
 
 #ifdef INCLUDE_ASSIMP
+  void assimpTest(){
 
+  }
 #endif
 
 #ifdef INCLUDE_BULLET
-
+  void bulletTest(){
+  	
+  }
 #endif
 
-int main(){
+int main(int argc, char* argv[]){
 	#ifdef INCLUDE_GLFW
 		glfwTest();
 	#endif
 
+	#ifdef INCLUDE_GLAD
+		gladTest();
+	#endif
+
 	#ifdef INCLUDE_GLM
 		glmTest();
+	#endif
+
+	#ifdef INCLUDE_STB
+		stbImageTest();
+	#endif
+
+	#ifdef INCLUDE_OPENAL
+		openAlTest();
+	#endif
+
+	#ifdef INCLUDE_CXXOPTS
+		cxxoptsTest(argc, argv);
+	#endif
+
+	#ifdef INCLUDE_ASSIMP
+		assimpTest();
+	#endif
+
+	#ifdef INCLUDE_BULLET
+		bulletTest();
 	#endif
 
 
