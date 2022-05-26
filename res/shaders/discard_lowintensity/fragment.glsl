2493972e7c5974f442de84747a50af0c26bde301
BradfordMedeiros <bradmedeiros0@gmail.com>
2022-05-26 09:29:36 -0700

    shaders -> commit uncommitted files

--- res/shaders/discard_lowintensity/fragment.glsl
+++ res/shaders/discard_lowintensity/fragment.glsl
@@ -10,7 +10,7 @@ uniform sampler2D maintexture;
 uniform sampler2D emissionTexture;
 uniform sampler2D opacityTexture;
 
-uniform vec3 tint;
+uniform vec4 tint;
 uniform vec3 cameraPosition;
 
 uniform bool enableDiffuse;
@@ -33,63 +33,12 @@ const float emissionAmount = 1;
 uniform float discardTexAmount;
 
 void main(){
-  if (tint.r < 0.1){
-    FragColor = vec4(tint.r, tint.g, tint.b, 1.0);
-  }else{
+  if (tint.b > 0.8){
+    discard;
+  }
     vec2 adjustedTexCoord = TexCoord + textureOffset;
     vec4 diffuseColor = texture(maintexture, vec2(adjustedTexCoord.x, -adjustedTexCoord.y));
-    vec4 emissionColor = texture(emissionTexture, vec2(adjustedTexCoord.x, -adjustedTexCoord.y));
-    vec4 opacityColor = texture(opacityTexture, vec2(adjustedTexCoord.x, -adjustedTexCoord.y));
-
-    bool discardTexture = hasOpacityTexture && opacityColor.r < discardTexAmount;     // This is being derived from emission map but going to use different map (in progress)
-
-    vec4 texColor;
-    if (discardTexture){
-        discard;
-    }else{
-        texColor = diffuseColor + emissionAmount * (hasEmissionTexture ? emissionColor : vec4(0, 0, 0, 0));
-    }
-
-    if (texColor.a < 0.1){
-      discard;
-    }
-
-    vec3 ambient = vec3(0.1, 0.1, 0.1);     
-    vec3 totalSpecular = vec3(0.2, 0.2, 0.2);
-    vec3 totalDiffuse  = vec3(0.2, 0.2, 0.2);
-    
-    for (int i = 0; i < min(numlights, MAX_LIGHTS); i++){
-        vec3 lightPos = lights[i];
-        vec3 lightDir = normalize(lightPos - FragPos);
-        vec3 normal = normalize(Normal);
-
-        float angle = dot(lightDir, normalize(-lightsdir[i]));
-        if (angle < 0.1){
-            continue;
-        }
-
-        vec3 diffuse = max(dot(normal, lightDir), 0.0) * lightscolor[i];
-
-        vec3 viewDir = normalize(cameraPosition - FragPos);
-        vec3 reflectDir = reflect(-lightDir, normal);  
-        vec3 specular = pow(max(dot(viewDir, reflectDir), 0.0), 32) * vec3(1.0, 1.0, 1.0);  
- 
-        float distanceToLight = length(lightPos - FragPos);
-        float attenuation = 1.0 / (constant + linear * distanceToLight + quadratic * (distanceToLight * distanceToLight));  
-
-        totalDiffuse = totalDiffuse + (attenuation * diffuse * lightscolor[i]);
-        totalSpecular = totalSpecular + (attenuation * specular * lightscolor[i]);
-    }
-
-    vec3 diffuseValue = enableDiffuse ? totalDiffuse : vec3(0, 0, 0);
-    vec3 specularValue = enableSpecular ? totalSpecular : vec3(0, 0, 0);
-    vec4 color = vec4(ambient + diffuseValue + specularValue, 1.0) * texColor;
-    float sum = color.x + color.y + color.z;
-    if (color.x > 0.1){
-        discard;
-    }
-
-    vec4 colorr = color * vec4(6, 6, 8, 1);
-    FragColor = vec4(colorr.xyz, 0.2);
-  }
+   
+    FragColor = tint * diffuseColor;
+  
 }
--- res/shaders/drawing/fragment.glsl
+++ res/shaders/drawing/fragment.glsl
@@ -4,8 +4,8 @@ out vec4 FragColor;
 in vec2 TexCoords;
 uniform sampler2D framebufferTexture;
 uniform float opacity;
-uniform vec3 tint;
+uniform vec4 tint;
 
 void main(){
-   FragColor = (vec4(tint.rgb, 1.0) * texture(framebufferTexture, TexCoords)) * vec4(1, 1, 1, opacity);
+   FragColor = (tint * texture(framebufferTexture, TexCoords)) * vec4(1, 1, 1, opacity);
 }
