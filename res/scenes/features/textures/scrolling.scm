c5bb547377c44919c991759ad99e374a55a33dc3
BradfordMedeiros <bradmedeiros0@gmail.com>
2022-05-26 09:36:33 -0700

    res -> more uncommitted files

--- res/scenes/features/textures/scrolling.scm
+++ res/scenes/features/textures/scrolling.scm
@@ -8,21 +8,24 @@
 
 (define xpos 0)
 (define (onFrame)
+  ;(format #t "scrolling on frame called!\n")
+  ;(format #t "x pos: ~a\n" xpos)
   (if (time-elapsed 0.01)
     (begin
       (set! lasttime (time-seconds))
-      (set! xpos (+ xpos 0.01))
+      (set! xpos (+ xpos 0.001))
       (if (> xpos 1) (set! xpos 0))
+      ;(format #t "setting texture offset: ~a\n" (string-append (number->string xpos) " 0"))
       (gameobj-setattr! 
-        mainobj
+        meshObj
         (list
-          (list "position" (list xpos 0 0))
+          (list "textureoffset" (string-append (number->string xpos) " 0"))
         )
       )
       (gameobj-setattr! 
         meshObj
         (list
-          (list "color" (list (* 10 xpos) 1 1))
+          (list "color" (list (* 10 xpos) 1 1 1))
         )
       )
     )
