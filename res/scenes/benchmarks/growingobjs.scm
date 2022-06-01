86fce5ce0536cbb5ba65a69810cb71e483efb4bc
BradfordMedeiros <bradmedeiros0@gmail.com>
2022-05-31 22:00:19 -0700

    benchmark scene -> arg for rate and model

--- res/scenes/benchmarks/growingobjs.scm
+++ res/scenes/benchmarks/growingobjs.scm
@@ -1,8 +1,12 @@
 
+(define modelpath (if (args "modelpath") (args "modelpath") "./res/models/electricbox/electricbox.obj"))
+(format #t "Growing objs: using model: ~a\n" modelpath)
+(define rate (if (args "rate") (string->number (args "rate")) 1))
+
 (define objid 0)
 (define (make-box)
   (mk-obj-attr (string-append "obj-" (number->string objid)) (list
-    (list "mesh"     "./res/models/electricbox/electricbox.obj")
+    (list "mesh"     modelpath)
     (list "position" (list (modulo objid 100) (quotient objid 100) 0))
   ))
   (set! objid (+ objid 2))
@@ -11,7 +15,7 @@
 (define last-time 0)
 (define (onFrame)
   (define now (time-seconds))
-  (if (> (- now last-time) 1)
+  (if (> (- now last-time) rate)
     (begin
       (set! last-time now)
       (make-box)
