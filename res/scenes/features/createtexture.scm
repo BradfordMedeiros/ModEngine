37276d4d67bbab40bd5c118308f8e55f71ab658e
BradfordMedeiros <bradmedeiros0@gmail.com>
2022-06-07 09:35:40 -0700

    scene -> feature -> create texture plot fps graph

--- res/scenes/features/createtexture.scm
+++ res/scenes/features/createtexture.scm
@@ -13,17 +13,47 @@
     		(list "texture" "graphs-testplot")
     	)
     )
-
 )
 
 
 
-; 1/Plane:texture:graphs-testplot
+(define xRangeSecs 100)
+(define yRangeFPS 500)
+(define (convertX value) (- (* 2 (/ value xRangeSecs)) 1))
+(define (convertY value) (- (* 2 (/ value yRangeFPS )) 1))
 
+
+(define pointX #f)
+(define pointY #f)
+(define (plotPoint x y)
+	(if (and pointX pointY)
+		(draw-line (list pointX pointY 0) (list x y 0) #t textureId)
+	)
+	(set! pointX x)
+	(set! pointY y)
+)
+
+(define minDrawingTime 0.1)
+(define lastTime (time-seconds))
+(define (onFrame)
+	(define now (time-seconds))
+	(define diff (- now lastTime))
+	(if (> diff minDrawingTime)
+		(begin
+			(set! lastTime now)
+      (let* ((currSec (time-seconds)) (fps (runstat "fps")) (coordX (convertX currSec)) (coordY (convertY fps)))
+				;(format #t "(time = ~a[~a] , fps = ~a[~a])" currSec  coordX fps coordY)
+				(plotPoint coordX coordY)
+			)
+		)
+	)
+)
+
+(define textureId #f)
 (define texturename "graphs-testplot")
+(set! textureId (create-texture texturename 100 100))
+
 (define (onKeyChar codepoint)
-	(format #t "key char: ~a\n" codepoint)
-	(if (equal? codepoint 44) (create-texture texturename 1000 1000)) ; ,
 	(if (equal? codepoint 46) (free-texture texturename))           ; . 
 	(if (equal? codepoint 47) (create-obj))                         ; / 
 )
