991d8f54f380d4e1e1ff1041ab94ccc8a2095877
BradfordMedeiros <bradmedeiros0@gmail.com>
2022-06-07 21:40:02 -0700

    benchstats -> add todo comment

--- src/benchstats.h
+++ src/benchstats.h
@@ -4,6 +4,8 @@
 #include <vector>
 #include "./common/util.h"
 
+// TODO -> this should be how all the values in benchmark come from
+// then benchmark would just be the aggregator 
 unsigned int statName(std::string name);
 void registerStat(unsigned int stat, AttributeValue amount);
 AttributeValue statValue(unsigned int);
