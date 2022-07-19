a00eed3d99aa9141731b408921f9163283b5d68b
BradfordMedeiros <bradmedeiros0@gmail.com>
2022-07-19 12:53:05 -0700

    editor -> numeric -> use textbox wip

--- res/scenes/editor/dock/details_numeric.php
+++ res/scenes/editor/dock/details_numeric.php
@@ -4,32 +4,31 @@
   $value = $data["value"];
   $numericElements = [ $keyname ];
 
-  for ($i = 0; $i < count($value); $i++){
-    $floatLabelElementName =  ")" . $unique_control_id . "_" . "numeric_" . $i . "_float";
-    createElement($floatLabelElementName, $default_key, [ "value" => $value[$i]["name"] ]);
-    $controlType = $value[$i]["type"];
+  for ($x = 0; $x < count($value); $x++){
+    $floatLabelElementName =  ")" . $unique_control_id . "_" . "numeric_" . $x . "_float";
+    createElement($floatLabelElementName, $default_key, [ "value" => $value[$x]["name"] ]);
+    $controlType = $value[$x]["type"];
 
     $managedElements = [ $floatLabelElementName ];
-    $hasBinding = !is_string($value[$i]);
+    $hasBinding = !is_string($value[$x]);
     $textValue = "";
     if (! $hasBinding){
-      $textValue = $value[$i]["value"];
+      $textValue = $value[$x]["value"];
     }
     $attrValues = [ 
       "value" => $textValue, 
       "details-editabletext" => "true",
     ];
     if ($hasBinding){
-      $attrValues["details-binding"] = $value[$i]["value"]["binding"];
-      if (array_key_exists("binding-index", $value[$i]["value"])){
-        $attrValues["details-binding-index"] = $value[$i]["value"]["binding-index"];
+      $attrValues["details-binding"] = $value[$x]["value"]["binding"];
+      if (array_key_exists("binding-index", $value[$x]["value"])){
+        $attrValues["details-binding-index"] = $value[$x]["value"]["binding-index"];
       }
     }
 
     if ($controlType == "float"){
-      $floatValueElementName =  ")" . $unique_control_id . "_" . "numeric"  . $i . "_value";
-      if (array_key_exists("type", $value[$i]["value"])){
-        $type = $value[$i]["value"]["type"];
+      if (array_key_exists("type", $value[$x]["value"])){
+        $type = $value[$x]["value"]["type"];
         if ($type != "number" && $type != "positive-number"){
           print("invalid numeric type");
           exit(1);
@@ -39,10 +38,13 @@
         $attrValues["details-editable-type"] = "number";
       }
 
-      createElement($floatValueElementName, $default_value, $attrValues);
-      array_push($managedElements, $floatValueElementName);
+      $holdername = "(" . $unique_control_id . "_numeric_textfield_holder_" . $x;
+      $valuename = ")numeric_text_" . $unique_control_id . "_" . $x;
+
+      createTextbox($holdername, $valuename, false, [ "value" => "placeholder" ], $styles);
+      array_push($managedElements, $holdername);
     }else if ($controlType == "slider"){
-      $sliderElementName =  "_" . $unique_control_id . "_" . "numeric"  . $i . "_value";
+      $sliderElementName =  "_" . $unique_control_id . "_" . "numeric"  . $x . "_value";
       $attrValues["scale"] = "0.3 0.02 0.02";
       $attrValues["onslide"] = "details-editable-slide";
       $attrValues["backpaneltint"] = "0.3 0.3 0.3 1";
@@ -52,8 +54,8 @@
       print("Invalid control type");
       exit(1);
     }
-    $floatElementName = "(" . $unique_control_id . "_" . "numeric_" . $i;
-    createElement($floatElementName, $default_key, [ "min-spacing" => "0.08", "margin" => "0.02", "minwidth" => "0.44", "tint" => "0.2 0.2 0.2 1", "backpanel" => "true", "elements" => implode(",", $managedElements) ]);
+    $floatElementName = "(" . $unique_control_id . "_" . "numeric_" . $x;
+    createElement($floatElementName, $default_key, [ "min-spacing" => "0.08", "margin" => "0.02", "minwidth" => "0.44", "tint" => "0.2 0.2 0.2 1", "backpanel" => "true", "elements" => implode(",", $managedElements), "position" => "0 0 " . $depth[29] ]);
     array_push($numericElements, $floatElementName);
   }
 
