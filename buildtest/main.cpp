f086ff769d280877e492492a6b5651f38df80731
BradfordMedeiros <bradmedeiros0@gmail.com>
2022-07-25 18:15:51 -0700

    cmake / build test -> include freetype

--- buildtest/main.cpp
+++ buildtest/main.cpp
@@ -148,8 +148,20 @@ void testpass(std::string target){
 #endif
 
 #ifdef INCLUDE_FREETYPE
+  #include <ft2build.h>
+  #include FT_FREETYPE_H  
+
   void freetypeTest(){
-    assert(false);
+    FT_Library ft;
+    if (FT_Init_FreeType(&ft)){
+        std::cout << "ERROR::FREETYPE: Could not init FreeType Library" << std::endl;
+        assert(false);
+    }
+    FT_Face face;
+    if (FT_New_Face(ft, "res/fonts/testfont.ttf", 0, &face)){
+        std::cout << "ERROR::FREETYPE: Failed to load font" << std::endl;  
+        assert(false);
+    }
     testpass("FreeType");
   }
 #endif
