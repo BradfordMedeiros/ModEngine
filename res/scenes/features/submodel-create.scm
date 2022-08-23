fde8ae10d152f02ed6d34c4a855a210f6947ddeb
BradfordMedeiros <bradmedeiros0@gmail.com>
2022-08-23 16:36:09 -0700

    feature scene -> mkobj create with submodel attr specified

---
+++ res/scenes/features/submodel-create.scm
@@ -0,0 +1,22 @@
+(define madeobj #f)
+(define (onKey key scancode action mods)
+	(format #t "key is: ~a\n" key)
+	(if (and (equal? action 1) (equal? key 257))
+		(begin
+			(format #t "making someobj2\n")
+			(if (not madeobj)
+				(begin
+					(mk-obj-attr "someobj2"     
+  					(list
+  						(list "position" (list 0 2 0))
+  					  (list "texture" "./res/textures/grid.png")
+  					  (list "mesh" "../gameresources/build/debug/twospike.gltf")
+  					  (list "someobj2/Cube.001" "tint" (list 0 1 0 1))
+  					)
+					)	
+					(set! madeobj #t)
+				)
+			)	
+		)
+	)
+)
