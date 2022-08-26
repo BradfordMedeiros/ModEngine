0e3914fea79f836798d0f7b76bb29896d0f56e44
BradfordMedeiros <bradmedeiros0@gmail.com>
2022-08-26 11:22:23 -0700

    include framebuffer with extra id using layout = 2

--- res/shaders/selection/fragment.glsl
+++ res/shaders/selection/fragment.glsl
@@ -2,11 +2,14 @@
 
 layout(location = 0) out vec4 FragColor;
 layout(location = 1) out vec2 UVCoords;
+layout(location = 2) out vec4 FragColor2;
 
 uniform vec4 encodedid;
+uniform vec4 encodedid2;
 in vec2 TexCoord;
 
 void main(){
   FragColor = vec4(encodedid.x, encodedid.y, encodedid.z, encodedid.w);
+  FragColor2 = vec4(encodedid2.x, encodedid2.y, encodedid2.z, encodedid2.w);
   UVCoords = TexCoord;
 }
