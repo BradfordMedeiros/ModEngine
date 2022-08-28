984be9d23de59562f1d110456a2797f2985fbdd3
BradfordMedeiros <bradmedeiros0@gmail.com>
2022-08-28 15:39:12 -0700

    colorattachment 1 gets texture bound in render screenspace lines, assertion around tex sizes

--- src/main.cpp
+++ src/main.cpp
@@ -157,13 +157,16 @@ void applyPainting(objid id){
 
 void renderScreenspaceLines(Texture& texture, Texture texture2, bool shouldClear, glm::vec4 clearColor, std::optional<unsigned int> clearTextureId){
   auto texSize = getTextureSizeInfo(texture);
+  auto texSize2 = getTextureSizeInfo(texture);
+  modassert(texSize.width == texSize2.width && texSize.height == texSize2.height, "screenspace - invalid tex sizes");
+
   glViewport(0, 0, texSize.width, texSize.height);
   updateDepthTexturesSize(textureDepthTextures, 1, texSize.width, texSize.height); // wonder if this would be better off preallocated per gend texture?
   setActiveDepthTexture(fbo, textureDepthTextures, 0);
 
   glBindFramebuffer(GL_FRAMEBUFFER, fbo);
   glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, texture.textureId, 0);
-  //glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT1, GL_TEXTURE_2D,  texture2.textureId, 0);
+  glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT1, GL_TEXTURE_2D,  texture2.textureId, 0);
   
   glUseProgram(uiShaderProgram);
 
