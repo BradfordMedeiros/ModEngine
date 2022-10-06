e8b19a1502116f75629969147dcbcc346a299d78
BradfordMedeiros <bradmedeiros0@gmail.com>
2022-10-06 10:35:54 -0700

    scripts - save scene - save all scenes with save tag

--- res/scripts/save-scene.scm
+++ res/scripts/save-scene.scm
@@ -2,11 +2,21 @@
 (define lastSaveTime 0)
 (define saveTimeInSeconds 10)
 
+(define (scenesToSave)
+  (define scenes (list-scenes (list "save")))
+  scenes
+)
+
+(define (save sceneId)
+  (format #t "save scene: ~a\n" sceneId)
+  (save-scene #f sceneId)
+)
+
 (define (onFrame)
-  (define current-time (time-seconds))
+  (define current-time (time-seconds #t))
   (if (< saveTimeInSeconds (- current-time lastSaveTime)) 
     (begin
-      (save-scene)
+      (for-each save (scenesToSave))
       (set! lastSaveTime current-time)
       (display "SCRIPT - save-scene.SCM - saving scene")
     )
