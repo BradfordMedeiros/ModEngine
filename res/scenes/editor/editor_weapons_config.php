912fff3caddd6fb703f6371561784135b4fa3424
BradfordMedeiros <bradmedeiros0@gmail.com>
2022-11-07 23:14:20 -0800

    editor -> put all build files into build file instead of inplace source

--- res/scenes/editor/editor_weapons_config.php
+++ res/scenes/editor/editor_weapons_config.php
@@ -1,7 +1,7 @@
 <?php 
   $text_buttons = [
     ["value" => "hide", "dialogoption" => "HIDE"],
-    ["value" => "MOVEMENT", "dialogoption" => "./res/scenes/editor/dock/editor_movement.rawscene"],
-    ["value" => "WEAPONS", "dialogoption" => "./res/scenes/editor/dock/editor_weapons.rawscene"],
+    ["value" => "MOVEMENT", "dialogoption" => "./res/scenes/editor/build/dock/editor_movement.rawscene"],
+    ["value" => "WEAPONS", "dialogoption" => "./res/scenes/editor/build/dock/editor_weapons.rawscene"],
   ];
 ?>
