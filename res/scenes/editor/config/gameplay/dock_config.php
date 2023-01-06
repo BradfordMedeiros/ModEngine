dade3d1df2f481f046d4393c787fd36849ac003c
BradfordMedeiros <bradmedeiros0@gmail.com>
2023-01-05 22:59:06 -0800

    editor - mock gameplay config

--- res/scenes/editor/config/gameplay/dock_config.php
+++ res/scenes/editor/config/gameplay/dock_config.php
@@ -135,5 +135,77 @@
         ],
       ],
     ],
+    "volumes" => [
+      "items" => [
+        [
+          "type" => "label",
+          "data" => [
+            "key" => "Create Collision Volumes",
+            "readonly" => true,
+            "value" => [
+              "binding" => "false-binding-0",
+            ]
+          ],
+        ],
+        [
+          "type" => "label",
+          "data" => [
+            "key" => "Create Volume",
+            "readonly" => true,
+            "value" => [
+              "binding" => "false-binding-0",
+            ],
+            "action" => "create-volume", # not yet implemented
+            "tint" => "1 1 0 1",
+          ],
+        ],
+        [
+          "type" => "options",
+          "data" => [
+            "key" => "type", 
+            "options" => [
+              # bindings need to be corrected
+              [ "label" => "box", "binding" => "gameobj:type", "binding-on" => "point" ], #
+              [ "label" => "sphere", "binding" => "gameobj:type", "binding-on" => "spotlight" ],
+              [ "label" => "cylinder", "binding" => "gameobj:type", "binding-on" => "directional" ],
+            ],
+          ],
+        ],
+        [
+          "type" => "checkbox",
+          "data" => [
+            "key" => "Has Trigger", 
+            "value" => [
+              "binding" => "gameobj:dof",  
+              "binding-on" => "enabled",
+              "binding-off" => "disabled",
+            ],
+          ],
+        ],
+        [
+          "type" => "label",
+          "data" => [
+            "key" => "Trigger Name",
+            "value" => [
+              "binding" => "false-binding-0",
+            ]
+          ],
+        ],
+      ],
+    ],
+    "hud" => [
+      "items" => [
+        [
+          "type" => "label",
+          "data" => [
+            "key" => "Select Hud",
+            "readonly" => true,
+            "value" => [
+              "binding" => "false-binding-0",
+            ]
+          ],
+        ],
+      ],
+    ],
   ];
 ?>
--- res/scenes/editor/config/gameplay/editor_config.php
+++ res/scenes/editor/config/gameplay/editor_config.php
@@ -3,5 +3,7 @@
     ["value" => "hide", "dialogoption" => "HIDE"],
     ["value" => "MOVEMENT", "dialogoption" => "./res/scenes/editor/build/dock/editor_movement.rawscene"],
     ["value" => "WEAPONS", "dialogoption" => "./res/scenes/editor/build/dock/editor_weapons.rawscene"],
+    ["value" => "TRIGGERS", "dialogoption" => "./res/scenes/editor/build/dock/editor_volumes.rawscene"],
+    ["value" => "HUD", "dialogoption" => "./res/scenes/editor/build/dock/hud.rawscene"],
   ];
 ?>
