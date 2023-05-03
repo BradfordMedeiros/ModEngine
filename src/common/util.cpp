36a1abab84f5fd0042477ac60f24b031a13a8d5d
BradfordMedeiros <bradmedeiros0@gmail.com>
2023-05-02 23:42:49 -0700

    util - print mat4 in col major order

--- src/common/util.cpp
+++ src/common/util.cpp
@@ -227,23 +227,23 @@ std::string print(glm::quat quat){
 }
 std::string print(glm::mat4 mat){
   return "[" + 
-    std::to_string(mat[0][0]) + ", " + std::to_string(mat[0][1]) + ", " + std::to_string(mat[0][2]) + ", " + std::to_string(mat[0][3]) + "; " +
-    std::to_string(mat[1][0]) + ", " + std::to_string(mat[1][1]) + ", " + std::to_string(mat[1][2]) + ", " + std::to_string(mat[1][3]) + "; " +
-    std::to_string(mat[2][0]) + ", " + std::to_string(mat[2][1]) + ", " + std::to_string(mat[2][2]) + ", " + std::to_string(mat[2][3]) + "; " +
-    std::to_string(mat[3][0]) + ", " + std::to_string(mat[3][1]) + ", " + std::to_string(mat[3][2]) + ", " + std::to_string(mat[3][3]) + 
+    std::to_string(mat[0][0]) + ", " + std::to_string(mat[1][0]) + ", " + std::to_string(mat[2][0]) + ", " + std::to_string(mat[3][0]) + "; " +
+    std::to_string(mat[0][1]) + ", " + std::to_string(mat[1][1]) + ", " + std::to_string(mat[2][1]) + ", " + std::to_string(mat[3][1]) + "; " +
+    std::to_string(mat[0][2]) + ", " + std::to_string(mat[1][2]) + ", " + std::to_string(mat[2][2]) + ", " + std::to_string(mat[3][2]) + "; " +
+    std::to_string(mat[0][3]) + ", " + std::to_string(mat[1][3]) + ", " + std::to_string(mat[2][3]) + ", " + std::to_string(mat[3][3]) + 
   "]";
 }
 std::string print(glm::mat3 mat){
   return "[" + 
-    std::to_string(mat[0][0]) + ", " + std::to_string(mat[0][1]) + ", " + std::to_string(mat[0][2]) + "; " +
-    std::to_string(mat[1][0]) + ", " + std::to_string(mat[1][1]) + ", " + std::to_string(mat[1][2]) + "; " +
-    std::to_string(mat[2][0]) + ", " + std::to_string(mat[2][1]) + ", " + std::to_string(mat[2][2]) + "; " +
+    std::to_string(mat[0][0]) + ", " + std::to_string(mat[1][0]) + ", " + std::to_string(mat[2][0]) + "; " +
+    std::to_string(mat[0][1]) + ", " + std::to_string(mat[1][1]) + ", " + std::to_string(mat[2][1]) + "; " +
+    std::to_string(mat[0][2]) + ", " + std::to_string(mat[1][2]) + ", " + std::to_string(mat[2][2]) + "; " +
   "]";
 }
 std::string print(glm::mat2 mat){
   return "[" + 
-    std::to_string(mat[0][0]) + ", " + std::to_string(mat[0][1]) + "; " +
-    std::to_string(mat[1][0]) + ", " + std::to_string(mat[1][1]) + "; " +
+    std::to_string(mat[0][0]) + ", " + std::to_string(mat[1][0]) + "; " +
+    std::to_string(mat[0][1]) + ", " + std::to_string(mat[1][1]) + "; " +
   "]";
 }
 
