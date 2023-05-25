68ef41e2cc34e78b56ce4abbe6b98d90ef4b590c
BradfordMedeiros <bradmedeiros0@gmail.com>
2023-05-24 21:21:44 -0700

    filewatcher (linux specific) working, not hooked up to reloading resources yet

--- src/common/watch_file.cpp
+++ src/common/watch_file.cpp
@@ -1,56 +1,5 @@
 #include "./watch_file.h"
 
-
-// verify if directory or just file 
-
-#include <sys/select.h>
-
-void MonitorFileChanges(const std::string& filePath) {
-
-/*
-    // Prepare the file descriptor set for select
-
-
-    while (true) {
-        // Wait for events using select
-        int numReady = select(maxFd + 1, &fds, nullptr, nullptr, nullptr);
-        if (numReady == -1) {
-            break;
-        }
-
-        if (FD_ISSET(inotifyFd, &fds)) {
-            // Read and process inotify events
-            constexpr int bufferSize = 4096;
-            char buffer[bufferSize];
-
-            ssize_t bytesRead = read(inotifyFd, buffer, bufferSize);
-            if (bytesRead == -1) {
-                break;
-            }
-
-            char* ptr = buffer;
-            while (ptr < buffer + bytesRead) {
-                inotify_event* event = reinterpret_cast<inotify_event*>(ptr);
-
-                if (event->wd == watchDescriptor && (event->mask & IN_MODIFY)) {
-                    std::cout << "File modified: " << event->name << std::endl;
-                }
-
-                ptr += sizeof(inotify_event) + event->len;
-            }
-        }
-    }
-
-   ;*/
-}
-
-void closeWatch(std::optional<FileWatch> filewatch){
-    if (filewatch.has_value()){
-        inotify_rm_watch(filewatch.value().inotifyFd, filewatch.value().watchDescriptor);
-        close(filewatch.value().inotifyFd);
-    }
-}
-
 std::optional<FileWatch> watchFiles(std::string directory, float debouncePeriodSeconds){
     if (directory == ""){
 	   return std::nullopt;
@@ -78,48 +27,49 @@ std::optional<FileWatch> watchFiles(std::string directory, float debouncePeriodS
     FD_SET(inotifyFd, &fds);
     int maxFd = inotifyFd;
     filewatch.maxFd = maxFd;
-
-
-    while (true) {
-        // Wait for events using select
-        timeval timeout {
-            .tv_sec = 0,
-            .tv_usec = 0,
-        };
-        std::cout << "checking for select events" << std::endl;
-        int numReady = select(filewatch.maxFd + 1, &filewatch.fds, NULL, NULL, &timeout);
-        modassert(numReady != -1, "got -1 for select, should look up what this means");
-
-        int isSet = FD_ISSET(inotifyFd, &fds);
-        std::cout << "is set: " << isSet << std::endl;
-
-        if (isSet) {
-            // Read and process inotify events
-            constexpr int bufferSize = 4096;
-            char buffer[bufferSize];
-
-            ssize_t bytesRead = read(inotifyFd, buffer, bufferSize);
-            modassert(bytesRead != -1, "error reading inotify buffer");
-   
-            char* ptr = buffer;
-            while (ptr < buffer + bytesRead) {
-                inotify_event* event = reinterpret_cast<inotify_event*>(ptr);
-
-                if (event -> wd == filewatch.watchDescriptor) {
-                    std::cout << "File modified: " << event->name << std::endl;
-                }
-
-                ptr += sizeof(inotify_event) + event->len;
-            }
-        }
-    }
-    closeWatch(filewatch);
+    filewatch.fds = fds;
     return filewatch;
 }
 
+void getChangedFilesFromWatch(std::optional<FileWatch>& filewatchOpt, std::set<std::string>& changedFiles){
+    auto filewatch = filewatchOpt.value();
+    timeval timeout {
+        .tv_sec = 0,
+        .tv_usec = 0,
+    };
+    int numReady = select(filewatch.maxFd + 1, &filewatch.fds, NULL, NULL, &timeout);
+    modassert(numReady != -1, "got -1 for select, should look up what this means");
 
+    if (FD_ISSET(filewatch.inotifyFd, &filewatch.fds)) {
+        constexpr int bufferSize = 4096;
+        char buffer[bufferSize];
 
+        ssize_t bytesRead = read(filewatch.inotifyFd, buffer, bufferSize);
+        modassert(bytesRead != -1, "error reading inotify buffer");
    
+        char* ptr = buffer;
+        while (ptr < buffer + bytesRead) {
+            inotify_event* event = reinterpret_cast<inotify_event*>(ptr);
+            if (event -> wd == filewatch.watchDescriptor) {
+                changedFiles.insert(event -> name);
+            }
+            ptr += sizeof(inotify_event) + event->len;
+        }
+    }
+}
+
+void closeWatch(std::optional<FileWatch> filewatch){
+    if (filewatch.has_value()){
+        inotify_rm_watch(filewatch.value().inotifyFd, filewatch.value().watchDescriptor);
+        close(filewatch.value().inotifyFd);
+    }
+}
+
 std::set<std::string> pollChangedFiles(std::optional<FileWatch>& filewatch){
-	return { "one.png" , "one.mp3" };
+    if (!filewatch.has_value()){
+        return {};
+    }
+    std::set<std::string> changedFiles;
+    getChangedFilesFromWatch(filewatch, changedFiles);
+	return changedFiles;
 }
