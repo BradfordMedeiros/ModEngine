894832e4778d5885001d4196a0517ee9691f1e72
BradfordMedeiros <bradmedeiros0@gmail.com>
2023-10-23 23:15:10 -0700

    bullet debug drawer - assert as opposed to just exit on not implemented methods, override keyword on overloaded methods

--- src/scene/bulletdebug.cpp
+++ src/scene/bulletdebug.cpp
@@ -1,7 +1,7 @@
 #include "./bulletdebug.h"
 
 BulletDebugDrawer::BulletDebugDrawer(int32_t(*drawLine)(glm::vec3 fromPos, glm::vec3 toPos, bool permaline, int32_t owner)){
-  this -> setDebugMode(btIDebugDraw::DBG_DrawWireframe);
+  this -> setDebugMode(btIDebugDraw::DBG_DrawWireframe );
   this -> debugDrawLine = drawLine; 
 }
 BulletDebugDrawer::~BulletDebugDrawer(){}
@@ -15,21 +15,21 @@ void BulletDebugDrawer::drawLine(const btVector3& from, const btVector3& to, con
 }
 void BulletDebugDrawer::drawSphere(const btVector3& p, btScalar radius, const btVector3& color){
   std::cout << "CRITICAL DEBUG WARNING: DRAW SPHERE: not yet implemented" << std::endl;
-  exit(1);  
+  assert(false);
 }
 void BulletDebugDrawer::drawTriangle(const btVector3& a, const btVector3& b, const btVector3& c, const btVector3& color, btScalar alpha){
   std::cout << "CRITICAL DEBUG WARNING: DRAW TRIANGLE: not yet implemented" << std::endl;
-  exit(1);  
+  assert(false);
 }
 void BulletDebugDrawer::drawContactPoint(const btVector3& PointOnB, const btVector3& normalOnB, btScalar distance, int lifeTime, const btVector3& color){
   std::cout << "CRITICAL DEBUG WARNING: DRAW CONTACT POINT: not yet implemented" << std::endl;
-  exit(1);
+  assert(false);
 }
 void BulletDebugDrawer::reportErrorWarning(const char* warningString){
   std::cout << "BULLET DEBUG WARNING: REPORT ERROR WARNING: " << warningString << std::endl;
-  exit(1);
+  assert(false);
 }
 void BulletDebugDrawer::draw3dText(const btVector3& location, const char* textString){
   std::cout << "CRITICAL DEBUG WARNING: not yet implemented" << std::endl;
-  exit(1);
+  assert(false);
 }
--- src/scene/bulletdebug.h
+++ src/scene/bulletdebug.h
@@ -12,15 +12,15 @@ class BulletDebugDrawer : public btIDebugDraw {
 public:
   BulletDebugDrawer(int32_t (*drawLine)(glm::vec3 fromPos, glm::vec3 toPos, bool permaline, int32_t owner));
   virtual ~BulletDebugDrawer();
-  virtual void drawLine(const btVector3& from, const btVector3& to, const btVector3& fromColor, const btVector3& toColor);
-  virtual void drawLine(const btVector3& from, const btVector3& to, const btVector3& color);
-  virtual void drawSphere(const btVector3& p, btScalar radius, const btVector3& color);
-  virtual void drawTriangle(const btVector3& a, const btVector3& b, const btVector3& c, const btVector3& color, btScalar alpha);
-  virtual void drawContactPoint(const btVector3& PointOnB, const btVector3& normalOnB, btScalar distance, int lifeTime, const btVector3& color);
-  virtual void reportErrorWarning(const char* warningString);
-  virtual void draw3dText(const btVector3& location, const char* textString);
-  virtual void setDebugMode(int debugMode) { this -> m_debugMode = debugMode; }
-  virtual int getDebugMode() const { return m_debugMode; }
+  virtual void drawLine(const btVector3& from, const btVector3& to, const btVector3& fromColor, const btVector3& toColor) override;
+  virtual void drawLine(const btVector3& from, const btVector3& to, const btVector3& color) override;
+  virtual void drawSphere(const btVector3& p, btScalar radius, const btVector3& color) override;
+  virtual void drawTriangle(const btVector3& a, const btVector3& b, const btVector3& c, const btVector3& color, btScalar alpha) override;
+  virtual void drawContactPoint(const btVector3& PointOnB, const btVector3& normalOnB, btScalar distance, int lifeTime, const btVector3& color) override;
+  virtual void reportErrorWarning(const char* warningString) override;
+  virtual void draw3dText(const btVector3& location, const char* textString) override;
+  virtual void setDebugMode(int debugMode) override { this -> m_debugMode = debugMode; }
+  virtual int getDebugMode() const override { return m_debugMode; }
 };
 
 #endif
