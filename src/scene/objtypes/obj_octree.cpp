f56159cc3f5bca8746d6f9db70bd19691917324f
BradfordMedeiros <bradmedeiros0@gmail.com>
2024-01-17 20:27:05 -0800

    -

--- src/scene/objtypes/obj_octree.cpp
+++ src/scene/objtypes/obj_octree.cpp
@@ -73,15 +73,73 @@ Octree subdividedOne {
 Octree testOctree = subdividedOne;
 
 
+OctreeDivision* getOctreeCell(Octree& octree, int x, int y, int z, int division){
+  return NULL;
+}
+
+int indexForSubdivision(int x, int y, int z, int sourceSubdivision, int targetSubdivision){
+  modassert(targetSubdivision <= sourceSubdivision, "target subdivision needs to be same or less than source");
+  int numCells = glm::pow(2, sourceSubdivision - targetSubdivision);
+  int offsetX = x / numCells;
+  int offsetY = y / numCells;
+  int offsetZ = z / numCells;
+  int index = 0;
+  modassert(index >= 0 && index < 8, "index must be between 0 and 8");
+  return index;
+}
+// This addresss the octree as if it's a voxel style grid
+// should be responsible for figuring out the proper octree representation
+void writeOctreeCell(Octree& octree, int x, int y, int z, int subdivision, bool filled){
+  // turn x y z w/ subdivision into a 
+  // path through the current octree
+
+  // for a given subdivision, we can calculate the ratio of x y z
+  // eg (1, 0, 0) w/ subdivision 1 
+  // 0.5 0 0
+  // eg (1, 0, 0) w/ subdivision 2
+  // 0.25 0 0  
+
+  std::cout << "octree trying to find: " << x << ", " << y << ", " << z << std::endl;
+  int xIndex = x;
+  for (int i = subdivision; i >= 0; i--){
+    xIndex = xIndex / 2;
+    std::cout << "octree index: " << xIndex << std::endl;
+  }
+
+  // path => coord / 2 
+
+  // subdivision 2 => 8
+
+
+
+  int maxValue = glm::pow(2, subdivision);
+
+  // problem: convert this number to an index at each subdivision level
+  // sub 0
+  //  (0, 0) => [0]
+
+  // sub 1
+  // (0, 0) => [0, 0]
+  // (1, 0) => [0, 1]
+  // (0, 1) => [0, 2]
+  // (1, 1) => [0, 3]
+
+  // sub 2 
+  // (0, 0) => [0, 0]
+  // (1, 0) => []
+  // (2, 0) => 
+  // (3, 0) => 
+}
+
 
 /*Mesh createNavmeshFromPointList(std::vector<glm::vec3> points, ){
 
 }*/
 
-Vertex createVertex2(glm::vec3 position, glm::vec2 texCoords){
+Vertex createVertex2(glm::vec3 position, glm::vec2 texCoords, glm::vec3 normal){
   Vertex vertex {
     .position = position,
-    .normal = glm::vec3(0.f, 0.f, 0.f),  // TODO - calculate normal properly
+    .normal = normal,
     .texCoords = texCoords,
   };
   for (int i = 0; i < NUM_BONES_PER_VERTEX; i++){
@@ -181,6 +239,7 @@ void addOctreeLevel(std::vector<glm::vec3>& points, glm::vec3 rootPos, OctreeDiv
   }else if (octreeDivision.filled){
     addCubePoints(points, size, rootPos);
   }
+
 }
 
 Mesh createOctreeMesh(ObjectTypeUtil& util){
@@ -192,23 +251,20 @@ Mesh createOctreeMesh(ObjectTypeUtil& util){
   std::cout << "adding octree end" << std::endl;
 
 
+  writeOctreeCell(testOctree, 1, 1, 0, 2, false);
+
+
   if (points.size() == 0){ // just hack for now 
     addCubePoints(points, 0.0001f, glm::vec3(0.f, 0.f, 0.f));
   }
 
-  for (int i = 0; i < points.size(); i++){
-    int triangleIndex = i % 3;
-    if (triangleIndex == 0){
-      vertices.push_back(createVertex2(points.at(i), glm::vec2(0.f, 0.f)));  // maybe the tex coords should just be calculated as a ratio to a fix texture
-      continue;
-    }else if (triangleIndex == 1){
-      vertices.push_back(createVertex2(points.at(i), glm::vec2(1.f, 0.f)));
-      continue;
-    }else if (triangleIndex == 2){
-      vertices.push_back(createVertex2(points.at(i), glm::vec2(0.f, 1.f)));
-      continue;
-    }
-    modassert(false, "invalid triangleIndex");
+  for (int i = 0; i < points.size(); i+=3){
+    glm::vec3 vec1 = points.at(i) - points.at(i + 1);
+    glm::vec3 vec2 = points.at(i) - points.at(i + 2);
+    auto normal = glm::cross(vec1, vec2); // think about sign better
+    vertices.push_back(createVertex2(points.at(i), glm::vec2(0.f, 0.f), normal));  // maybe the tex coords should just be calculated as a ratio to a fix texture
+    vertices.push_back(createVertex2(points.at(i + 1), glm::vec2(1.f, 0.f), normal));
+    vertices.push_back(createVertex2(points.at(i + 2), glm::vec2(0.f, 1.f), normal));
   }
   std::vector<unsigned int> indices;
   for (int i = 0; i < vertices.size(); i++){ // should be able to optimize this better...
