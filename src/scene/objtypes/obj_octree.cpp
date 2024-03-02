6b4dfc6f99276d597af49f3eaffada08890af42d
BradfordMedeiros <bradmedeiros0@gmail.com>
2024-03-02 01:05:05 -0800

    join sparse shapes - 2 iterations

--- src/scene/objtypes/obj_octree.cpp
+++ src/scene/objtypes/obj_octree.cpp
@@ -1226,106 +1226,102 @@ std::vector<SparseShape> joinSparseShapes(std::vector<SparseShape>& shapes){
 
   std::vector<SparseShape> joinedShapes;
 
-
-  for (int i = 0; i < shapes.size(); i++){
-    for (int j = i + 1; j < shapes.size(); j++){
-      SparseShape& shape1 = shapes.at(i);
-      SparseShape& shape2 = shapes.at(j);
-      if (shape1.deleted || shape2.deleted){
-        continue;
+  for (int iterations = 0; iterations < 2; iterations++){
+    for (int i = 0; i < shapes.size(); i++){
+      for (int j = i + 1; j < shapes.size(); j++){
+        SparseShape& shape1 = shapes.at(i);
+        SparseShape& shape2 = shapes.at(j);
+        if (shape1.deleted || shape2.deleted){
+          continue;
+        }
+        // if shape2 can fit into shape1 in other y and z dimensions, and is right next ot it on the x
+        // or vice versa
+        if (
+          (shape2.minY == shape1.minY && shape2.maxY == shape1.maxY &&  shape2.minZ == shape1.minZ && shape2.maxZ == shape1.maxZ) || 
+          (shape1.minY == shape2.minY && shape1.maxY == shape2.maxY &&  shape1.minZ == shape2.minZ && shape1.maxZ == shape2.maxZ)
+        ){
+          std::cout << "found a candidate for joining: " << print(shape1) << ", " << print(shape2) << std::endl;
+          // if the shape is just in the other...shouldn't ever happen
+          if (shape2.minX >= shape1.minX && shape2.maxX <= shape1.maxX){
+            shape2.deleted = true;
+            modassert(false, "expected blocks being joined not to be completely within the other");
+          }
+          if (shape2.minX == shape1.maxX){
+            shape2.deleted = true;
+            shape1.maxX = shape2.maxX;
+          }
+          if (shape1.minX == shape2.maxX){
+            shape1.deleted = true;
+            shape2.maxX = shape1.maxX;
+          }
+        }
       }
+    }
 
-      // if shape2 can fit into shape1 in other y and z dimensions, and is right next ot it on the x
-      // or vice versa
-      if (
-        (shape2.minY == shape1.minY && shape2.maxY == shape1.maxY &&  shape2.minZ == shape1.minZ && shape2.maxZ == shape1.maxZ) || 
-        (shape1.minY == shape2.minY && shape1.maxY == shape2.maxY &&  shape1.minZ == shape2.minZ && shape1.maxZ == shape2.maxZ)
-      ){
-        std::cout << "found a candidate for joining: " << print(shape1) << ", " << print(shape2) << std::endl;
-        // if the shape is just in the other...shouldn't ever happen
-        if (shape2.minX >= shape1.minX && shape2.maxX <= shape1.maxX){
-          shape2.deleted = true;
-          modassert(false, "expected blocks being joined not to be completely within the other");
+    // join over y
+    for (int i = 0; i < shapes.size(); i++){
+      for (int j = i + 1; j < shapes.size(); j++){
+        SparseShape& shape1 = shapes.at(i);
+        SparseShape& shape2 = shapes.at(j);
+        if (shape1.deleted || shape2.deleted){
+          continue;
         }
+        // if shape2 can fit into shape1 in other y and z dimensions, and is right next ot it on the x
+        // or vice versa
+        if (
+          (shape2.minX == shape1.minX && shape2.maxX == shape1.maxX &&  shape2.minZ == shape1.minZ && shape2.maxZ == shape1.maxZ) || 
+          (shape1.minX == shape2.minX && shape1.maxX == shape2.maxX &&  shape1.minZ == shape2.minZ && shape1.maxZ == shape2.maxZ)
+        ){
+          std::cout << "found a candidate for joining: " << print(shape1) << ", " << print(shape2) << std::endl;
+          // if the shape is just in the other...shouldn't ever happen
+          if (shape2.minY >= shape1.minY && shape2.maxY <= shape1.maxY){
+            shape2.deleted = true;
+            modassert(false, "expected blocks being joined not to be completely within the other");
+          }
+          if (shape2.minY == shape1.maxY){
+            shape2.deleted = true;
+            shape1.maxY = shape2.maxY;
+          }
+          if (shape1.minY == shape2.maxY){
+            shape1.deleted = true;
+            shape2.maxY = shape1.maxY;
+          }
+        }
+      }
+    }
 
-        if (shape2.minX == shape1.maxX){
-          shape2.deleted = true;
-          shape1.maxX = shape2.maxX;
+    // join over z
+    for (int i = 0; i < shapes.size(); i++){
+      for (int j = i + 1; j < shapes.size(); j++){
+        SparseShape& shape1 = shapes.at(i);
+        SparseShape& shape2 = shapes.at(j);
+        if (shape1.deleted || shape2.deleted){
+          continue;
         }
-        if (shape1.minX == shape2.maxX){
-          shape1.deleted = true;
-          shape2.maxX = shape1.maxX;
+        // if shape2 can fit into shape1 in other y and z dimensions, and is right next ot it on the x
+        // or vice versa
+        if (
+          (shape2.minX == shape1.minX && shape2.maxX == shape1.maxX &&  shape2.minY == shape1.minY && shape2.maxY == shape1.maxY) || 
+          (shape1.minX == shape2.minX && shape1.maxX == shape2.maxX &&  shape1.minY == shape2.minY && shape1.maxY == shape2.maxY)
+        ){
+          std::cout << "found a candidate for joining: " << print(shape1) << ", " << print(shape2) << std::endl;
+          // if the shape is just in the other...shouldn't ever happen
+          if (shape2.minZ >= shape1.minZ && shape2.maxZ <= shape1.maxZ){
+            shape2.deleted = true;
+            modassert(false, "expected blocks being joined not to be completely within the other");
+          }
+          if (shape2.minZ == shape1.maxZ){
+            shape2.deleted = true;
+            shape1.maxZ = shape2.maxZ;
+          }
+          if (shape1.minZ == shape2.maxZ){
+            shape1.deleted = true;
+            shape2.maxZ = shape1.maxZ;
+          }
         }
       }
     }
   }
-
-  // join over y
-  for (int i = 0; i < shapes.size(); i++){
-    for (int j = i + 1; j < shapes.size(); j++){
-      SparseShape& shape1 = shapes.at(i);
-      SparseShape& shape2 = shapes.at(j);
-      if (shape1.deleted || shape2.deleted){
-        continue;
-      }
-      // if shape2 can fit into shape1 in other y and z dimensions, and is right next ot it on the x
-      // or vice versa
-      if (
-        (shape2.minX == shape1.minX && shape2.maxX == shape1.maxX &&  shape2.minZ == shape1.minZ && shape2.maxZ == shape1.maxZ) || 
-        (shape1.minX == shape2.minX && shape1.maxX == shape2.maxX &&  shape1.minZ == shape2.minZ && shape1.maxZ == shape2.maxZ)
-      ){
-        std::cout << "found a candidate for joining: " << print(shape1) << ", " << print(shape2) << std::endl;
-        // if the shape is just in the other...shouldn't ever happen
-        if (shape2.minY >= shape1.minY && shape2.maxY <= shape1.maxY){
-          shape2.deleted = true;
-          modassert(false, "expected blocks being joined not to be completely within the other");
-        }
-
-        if (shape2.minY == shape1.maxY){
-          shape2.deleted = true;
-          shape1.maxY = shape2.maxY;
-        }
-        if (shape1.minY == shape2.maxY){
-          shape1.deleted = true;
-          shape2.maxY = shape1.maxY;
-        }
-      }
-    }
-  }
-
-  // join over z
-  for (int i = 0; i < shapes.size(); i++){
-    for (int j = i + 1; j < shapes.size(); j++){
-      SparseShape& shape1 = shapes.at(i);
-      SparseShape& shape2 = shapes.at(j);
-      if (shape1.deleted || shape2.deleted){
-        continue;
-      }
-      // if shape2 can fit into shape1 in other y and z dimensions, and is right next ot it on the x
-      // or vice versa
-      if (
-        (shape2.minX == shape1.minX && shape2.maxX == shape1.maxX &&  shape2.minY == shape1.minY && shape2.maxY == shape1.maxY) || 
-        (shape1.minX == shape2.minX && shape1.maxX == shape2.maxX &&  shape1.minY == shape2.minY && shape1.maxY == shape2.maxY)
-      ){
-        std::cout << "found a candidate for joining: " << print(shape1) << ", " << print(shape2) << std::endl;
-        // if the shape is just in the other...shouldn't ever happen
-        if (shape2.minZ >= shape1.minZ && shape2.maxZ <= shape1.maxZ){
-          shape2.deleted = true;
-          modassert(false, "expected blocks being joined not to be completely within the other");
-        }
-
-        if (shape2.minZ == shape1.maxZ){
-          shape2.deleted = true;
-          shape1.maxZ = shape2.maxZ;
-        }
-        if (shape1.minZ == shape2.maxZ){
-          shape1.deleted = true;
-          shape2.maxZ = shape1.maxZ;
-        }
-      }
-    }
-  }
-
 
 
   for (auto &shape : shapes){
