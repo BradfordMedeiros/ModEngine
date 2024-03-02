a2c79295de9546c2a535471ac80b1406630f9fc8
BradfordMedeiros <bradmedeiros0@gmail.com>
2024-03-01 21:30:32 -0800

    octee - physics - shape optimization - join over y and z.  Doesn't join different bounding box sizes yet, even if could based upon the relevant dimension.  Also doesn't optimize ramp shape types

--- src/scene/objtypes/obj_octree.cpp
+++ src/scene/objtypes/obj_octree.cpp
@@ -1225,6 +1225,8 @@ std::vector<SparseShape> joinSparseShapes(std::vector<SparseShape>& shapes){
   // join along x
 
   std::vector<SparseShape> joinedShapes;
+
+
   for (int i = 0; i < shapes.size(); i++){
     for (int j = i + 1; j < shapes.size(); j++){
       SparseShape& shape1 = shapes.at(i);
@@ -1254,15 +1256,77 @@ std::vector<SparseShape> joinSparseShapes(std::vector<SparseShape>& shapes){
           shape1.deleted = true;
           shape2.maxX = shape1.maxX;
         }
-
-      
-
-        // shapes overlap 
-
       }
-
     }
   }
+
+  // join over y
+  for (int i = 0; i < shapes.size(); i++){
+    for (int j = i + 1; j < shapes.size(); j++){
+      SparseShape& shape1 = shapes.at(i);
+      SparseShape& shape2 = shapes.at(j);
+      if (shape1.deleted || shape2.deleted){
+        continue;
+      }
+      // if shape2 can fit into shape1 in other y and z dimensions, and is right next ot it on the x
+      // or vice versa
+      if (
+        (shape2.minX == shape1.minX && shape2.maxX == shape1.maxX &&  shape2.minZ == shape1.minZ && shape2.maxZ == shape1.maxZ) || 
+        (shape1.minX == shape2.minX && shape1.maxX == shape2.maxX &&  shape1.minZ == shape2.minZ && shape1.maxZ == shape2.maxZ)
+      ){
+        std::cout << "found a candidate for joining: " << print(shape1) << ", " << print(shape2) << std::endl;
+        // if the shape is just in the other...shouldn't ever happen
+        if (shape2.minY >= shape1.minY && shape2.maxY <= shape1.maxY){
+          shape2.deleted = true;
+          modassert(false, "expected blocks being joined not to be completely within the other");
+        }
+
+        if (shape2.minY == shape1.maxY){
+          shape2.deleted = true;
+          shape1.maxY = shape2.maxY;
+        }
+        if (shape1.minY == shape2.maxY){
+          shape1.deleted = true;
+          shape2.maxY = shape1.maxY;
+        }
+      }
+    }
+  }
+
+  // join over z
+  for (int i = 0; i < shapes.size(); i++){
+    for (int j = i + 1; j < shapes.size(); j++){
+      SparseShape& shape1 = shapes.at(i);
+      SparseShape& shape2 = shapes.at(j);
+      if (shape1.deleted || shape2.deleted){
+        continue;
+      }
+      // if shape2 can fit into shape1 in other y and z dimensions, and is right next ot it on the x
+      // or vice versa
+      if (
+        (shape2.minX == shape1.minX && shape2.maxX == shape1.maxX &&  shape2.minY == shape1.minY && shape2.maxY == shape1.maxY) || 
+        (shape1.minX == shape2.minX && shape1.maxX == shape2.maxX &&  shape1.minY == shape2.minY && shape1.maxY == shape2.maxY)
+      ){
+        std::cout << "found a candidate for joining: " << print(shape1) << ", " << print(shape2) << std::endl;
+        // if the shape is just in the other...shouldn't ever happen
+        if (shape2.minZ >= shape1.minZ && shape2.maxZ <= shape1.maxZ){
+          shape2.deleted = true;
+          modassert(false, "expected blocks being joined not to be completely within the other");
+        }
+
+        if (shape2.minZ == shape1.maxZ){
+          shape2.deleted = true;
+          shape1.maxZ = shape2.maxZ;
+        }
+        if (shape1.minZ == shape2.maxZ){
+          shape1.deleted = true;
+          shape2.maxZ = shape1.maxZ;
+        }
+      }
+    }
+  }
+
+
 
   for (auto &shape : shapes){
     if (!shape.deleted){
@@ -1333,12 +1397,19 @@ std::vector<FinalShapeData> optimizePhysicsShapeData(std::vector<PhysicsShapeDat
     float subdivisionSizeX = glm::pow(0.5f, subdivisionLevels.x);
     float subdivisionSizeY = glm::pow(0.5f, subdivisionLevels.y);
     float subdivisionSizeZ = glm::pow(0.5f, subdivisionLevels.z);
-    glm::vec3 shapeSize(subdivisionSizeX, subdivisionSizeY, subdivisionSizeZ);
+
+    // max - minx is in terms of the smaller subdivisions, but the shapeSize is absolute to 1, so this corrects it
+    float multiplierX = (sparseShape.maxX - sparseShape.minX) / glm::pow(2, subdivisionSize - sparseShape.path.size());
+    float multiplierY = (sparseShape.maxY - sparseShape.minY) / glm::pow(2, subdivisionSize - sparseShape.path.size());
+    float multiplierZ = (sparseShape.maxZ - sparseShape.minZ) / glm::pow(2, subdivisionSize - sparseShape.path.size());
+
+    glm::vec3 adjustedSize(multiplierX * subdivisionSizeX, multiplierY * subdivisionSizeY, multiplierZ * subdivisionSizeZ);
+    //std::cout << "span x: " << spanX << ", maxsub = " << subdivisionSize << ", current size = " << sparseShape.path.size() << ", size = " << print(shapeSize) << ", adjusted = " << print(adjustedSize) << std::endl;
 
     optimizedShapes.push_back(FinalShapeData {
       .shape = sparseShape.shape,
       .position = calculatePosition(sparseShape.path),
-      .shapeSize = shapeSize,
+      .shapeSize = adjustedSize,
     });
   }
 
