ad7c6a3b8b892460046401da2769f93fdedcfd91
BradfordMedeiros <bradmedeiros0@gmail.com>
2024-03-21 23:16:12 -0700

    cleanup headers texturegen

--- src/scene/common/textures_gen.cpp
+++ src/scene/common/textures_gen.cpp
@@ -10,6 +10,14 @@ void genFramebufferTexture(unsigned int *texture, unsigned int resolutionX, unsi
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 }
 
+void updateDepthTexturesSize(unsigned int* textures, int numTextures, unsigned int resolutionX, unsigned int resolutionY){
+  for (int i = 0; i < numTextures; i++){
+    glBindTexture(GL_TEXTURE_2D, textures[i]);
+    // GL_DEPTH_COMPONENT32F
+    glTexImage2D(GL_TEXTURE_2D, 0,  GL_DEPTH_STENCIL, resolutionX, resolutionY, 0, GL_DEPTH_COMPONENT, GL_FLOAT, NULL);
+  }
+}
+
 void generateDepthTextures(unsigned int* textures, int numTextures, unsigned int resolutionX, unsigned int resolutionY){
   glGenTextures(numTextures, textures);
   for (int i = 0; i < numTextures; i++){
@@ -21,18 +29,19 @@ void generateDepthTextures(unsigned int* textures, int numTextures, unsigned int
     updateDepthTexturesSize(textures, numTextures, resolutionX, resolutionY);
   }
 }
-void updateDepthTexturesSize(unsigned int* textures, int numTextures, unsigned int resolutionX, unsigned int resolutionY){
-  for (int i = 0; i < numTextures; i++){
-    glBindTexture(GL_TEXTURE_2D, textures[i]);
-    // GL_DEPTH_COMPONENT32F
-    glTexImage2D(GL_TEXTURE_2D, 0,  GL_DEPTH_STENCIL, resolutionX, resolutionY, 0, GL_DEPTH_COMPONENT, GL_FLOAT, NULL);
-  }
-}
+
 void setActiveDepthTexture(unsigned int fbo, unsigned int* textures, int index){
   glBindFramebuffer(GL_FRAMEBUFFER, fbo);  
   unsigned int texture = textures[index];
   // GL_DEPTH_ATTACHMENT
   glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_TEXTURE_2D, texture, 0);
+}
+
+void updatePortalTexturesSize(unsigned int* portalTextures, int numPortalTextures, unsigned int resolutionX, unsigned int resolutionY){
+  for (int i = 0; i < numPortalTextures; i++){
+    glBindTexture(GL_TEXTURE_2D, portalTextures[i]);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, resolutionX, resolutionY, 0, GL_RGBA, GL_UNSIGNED_BYTE, NULL);   
+  }
 }
 
 void generatePortalTextures(unsigned int* portalTextures, int numPortalTextures, unsigned int resolutionX, unsigned int resolutionY){
@@ -46,12 +55,7 @@ void generatePortalTextures(unsigned int* portalTextures, int numPortalTextures,
     updatePortalTexturesSize(portalTextures, numPortalTextures, resolutionX, resolutionY);
   }
 }
-void updatePortalTexturesSize(unsigned int* portalTextures, int numPortalTextures, unsigned int resolutionX, unsigned int resolutionY){
-  for (int i = 0; i < numPortalTextures; i++){
-    glBindTexture(GL_TEXTURE_2D, portalTextures[i]);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, resolutionX, resolutionY, 0, GL_RGBA, GL_UNSIGNED_BYTE, NULL);   
-  }
-}
+
 
 Framebuffers generateFramebuffers(int resolutionX, int resolutionY){
   Framebuffers framebuffers;
--- src/scene/common/textures_gen.h
+++ src/scene/common/textures_gen.h
@@ -5,14 +5,9 @@
 #include <cstddef>
 #include <vector>
 
-void genFramebufferTexture(unsigned int *texture, unsigned int resolutionX, unsigned int resolutionY);
 
-void generateDepthTextures(unsigned int* textures, int numTextures, unsigned int resolutionX, unsigned int resolutionY);
 void updateDepthTexturesSize(unsigned int* textures, int numTextures, unsigned int resolutionX, unsigned int resolutionY);
 void setActiveDepthTexture(unsigned int fbo, unsigned int* textures, int index);
-
-void generatePortalTextures(unsigned int* portalTextures, int numPortalTextures, unsigned int resolutionX, unsigned int resolutionY);
-void updatePortalTexturesSize(unsigned int* portalTextures, int numPortalTextures, unsigned int resolutionX, unsigned int resolutionY);
 
 struct Framebuffers {
   unsigned int fbo;
