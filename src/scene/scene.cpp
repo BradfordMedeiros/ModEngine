6dd9dc242dc6d042c1f71fff0f90357db7f9a242
BradfordMedeiros <bradmedeiros0@gmail.com>
2025-01-21 20:55:13 -0800

    add physics body mesh - do not use phys offset

--- src/scene/scene.cpp
+++ src/scene/scene.cpp
@@ -62,6 +62,7 @@ std::optional<PhysicsInfo> getPhysicsInfoForGameObject(World& world, objid index
 
     for (Mesh* mesh : meshes){
       boundInfos.push_back(mesh -> boundInfo);
+      std::cout << "3 bound physics : " << obj.name << "  " << print(mesh -> boundInfo) << std::endl;
     }
     if (boundInfos.size() == 0){
       return std::nullopt;
@@ -70,7 +71,8 @@ std::optional<PhysicsInfo> getPhysicsInfoForGameObject(World& world, objid index
     auto fullTransform = fullTransformation(world.sandbox, index);
     boundInfo = getMaxUnionBoundingInfo(boundInfos);
     finalOffset = getOffsetForBoundInfo(boundInfo, fullTransform.scale);
-    std::cout << "2 physics : " << obj.name << ", index = " << index << ", group = " <<  getGameObjectH(world.sandbox, index).groupId << ", size = " << meshes.size() << ", offset: " << print(finalOffset.value()) << std::endl;
+    std::cout << "2 bound physics : " << obj.name << ", index = " << index << ", group = " <<  getGameObjectH(world.sandbox, index).groupId << ", size = " << meshes.size() << ", offset: " << print(finalOffset.value()) << std::endl;
+    std::cout << "4 bound physics : " << obj.name << ", " << print(boundInfo) << std::endl;
 
   }
 
@@ -261,6 +263,8 @@ PhysicsValue addPhysicsBody(World& world, objid id, bool initialLoad){
     modlog("2 physics", std::string("num verts: ") + std::to_string(verts.size()));
     assert(initialLoad);
     std::cout << "INFO: PHYSICS: ADDING SHAPE_EXACT RIGID BODY" << std::endl;
+
+    physicsInfo.offset = glm::vec3(0.f, 0.f, 0.f);
     rigidBody = addRigidBodyExact(
       world.physicsEnvironment,
       verts,
