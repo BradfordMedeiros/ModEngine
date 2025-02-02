95d0339d83aab383dfc2bbc0d271abb43df6854b
BradfordMedeiros <bradmedeiros0@gmail.com>
2025-02-02 15:51:28 -0800

    attach perf script

---
+++ perf/attach-perf.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# https://github.com/brendangregg/FlameGraph
+
+process_id=$(pgrep modengine)
+if [[ -z "$process_id" ]]
+then
+	echo "no process for modengine found"
+	exit 1
+fi
+
+(cd ../ && perf record -g --output=./perf/perf.data -i )
