ac3b4970bcbe8aeafb1d4e6e57be253d02b3d712
BradfordMedeiros <bradmedeiros0@gmail.com>
2025-03-09 15:16:12 -0700

    -

--- res/shaders/selection/fragment.glsl
+++ res/shaders/selection/fragment.glsl
@@ -7,13 +7,15 @@ uniform int textureid;
 uniform vec4 encodedid;
 uniform int voxelcellwidth;
 uniform vec4 tint;
+uniform vec3 cameraPosition;
+
 
 in vec2 TexCoord;
 
 
 
 void main(){
-  if (voxelcellwidth > 100 || tint.x > 100){  // removing selection shader anyway so 
+  if (voxelcellwidth > 100 || tint.x > 100 || cameraPosition.x == 10000){  // removing selection shader anyway so 
     discard;
   }
   EncodeId = vec4(encodedid.x, encodedid.y, encodedid.z, encodedid.w);
