5e640fd22b26c424e236b9c39d8a887c6c108788
BradfordMedeiros <bradmedeiros0@gmail.com>
2025-03-28 19:38:52 -0700

    rm ubuntu packing thing

--- packaging/modeditor/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-MODENGINE_BINARY={{MODENGINE_BINARY}}
-(cd $MODENGINE_BINARY && ./build/modengine --fullscreen -r ./res/scenes/editor/editor.rawscene)
--- packaging/uninstall.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-APP_DIRECTORY="$HOME/.local/share/applications/modengine/modeditor"
-rm -r "$APP_DIRECTORY"
