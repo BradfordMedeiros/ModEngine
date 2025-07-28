4d5cf0ae2ff2570d108d7f810d49a4d363f4a60a
brad <bradmedeiros0@gmail.com>
2025-07-27 21:31:22 -0700

    -

--- src/main.cpp
+++ src/main.cpp
@@ -1930,16 +1930,8 @@ int main(int argc, char* argv[]){
     }
 
 
-    if (state.enablePhysicsDebug){
-      debuggerDrawer -> setDebugMode(0);
-    }
-
     onWorldFrame(world, statistics.deltaTime, timePlayback.currentTime, state.enablePhysics, state.worldpaused, viewTransform, state.inputMode == ENABLED, false);
     cBindings.onFrameAfterUpdate();
-    
-    if (state.enablePhysicsDebug){
-      debuggerDrawer -> setDebugMode(1);
-    }
     onWorldFrame(world, statistics.deltaTime, timePlayback.currentTime, false, state.worldpaused, viewTransform, state.inputMode == ENABLED, true);
 
     handleChangedResourceFiles(pollChangedFiles(filewatch, glfwGetTime()));
