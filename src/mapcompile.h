c1d8f08c8fc75dc8a62d5367737a83df73ea365e
brad <bradmedeiros0@gmail.com>
2026-03-14 17:11:12 -0700

    code org - move compile map into file

---
+++ src/mapcompile.h
@@ -0,0 +1,18 @@
+#ifndef MOD_MAPCOMPILE
+#define MOD_MAPCOMPILE
+
+#include <vector>
+#include <string>
+#include <glm/glm.hpp>
+#include "./common/util.h"
+#include "./scene/common/map.h"
+
+typedef std::function<void(std::string& brushFileOut, MapData& mapData, Entity& entity, bool* shouldWrite, std::vector<GameobjAttributeOpts>& attributes, std::string* modelName)> CompileFn;
+typedef std::function<void(MapData& mapData, std::string& generatedScene)> FinalizeFn;
+struct CompileMapFns {
+  CompileFn compileFn;
+  FinalizeFn finalizeFn;
+};
+CompileMapFns getCompileMapForGame();
+
+#endif
