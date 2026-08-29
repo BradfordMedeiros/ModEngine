b9ee389dfb7d9e5b35ca6b0cdd28eb014910fb69
brad <bradmedeiros0@gmail.com>
2026-08-29 14:50:03 -0700

    crt shader stuff, just commented out for now

--- res/shaders/framebuffer/fragment.glsl
+++ res/shaders/framebuffer/fragment.glsl
@@ -38,8 +38,15 @@ void calculateFogEffect(in float depthAmount, out vec4 fogAmount){
 }
 
 void main(){
+  // CRT WARP EFFECT vec2 p = TexCoords * 2.0 - 1.0;
+  // CRT WARP EFFECT float curvature = 0.02;
+  // CRT WARP EFFECT p *= 1.0 + curvature * dot(p, p);
+  // CRT WARP EFFECT vec2 EffectTexCoords = p * 0.5 + 0.5;
+
+  vec2 EffectTexCoords = TexCoords;
+  
   if (enableDepthVisualization){
-    float depth  = texture(depthTexture, TexCoords).r;
+    float depth  = texture(depthTexture, EffectTexCoords).r;
     float z = depth * 2.0 - 1.0; 
     float depthAmount  = ((2.0 * near * far) / (far + near - z * (far - near))) / far;  // fraction of near/far
     FragColor = vec4(depthAmount, depthAmount, depthAmount, 1);
@@ -47,7 +54,7 @@ void main(){
   }
 
 
-  float depth  = texture(depthTexture, TexCoords).r;
+  float depth  = texture(depthTexture, EffectTexCoords).r;
   float z = depth * 2.0 - 1.0; 
   float depthAmount  = ((2.0 * near * far) / (far + near - z * (far - near))) / far;  // fraction of near/far
   
@@ -58,9 +65,9 @@ void main(){
   }
 
   if (enableBloom){
-    FragColor = fogEffect + (texture(framebufferTexture, TexCoords) + (bloomAmount * texture(bloomTexture, TexCoords)));
+    FragColor = fogEffect + (texture(framebufferTexture, EffectTexCoords) + (bloomAmount * texture(bloomTexture, TexCoords)));
   }else{
-    FragColor = fogEffect + texture(framebufferTexture, TexCoords);
+    FragColor = fogEffect + texture(framebufferTexture, EffectTexCoords);
   }
 
   // should restore after testing a bit more
@@ -73,9 +80,9 @@ void main(){
   vec2 offset = vec2(chromatic.x, chromatic.y);
 
   vec3 color = vec3(
-    texture(framebufferTexture, TexCoords + offset).r,
-    texture(framebufferTexture, TexCoords).g,
-    texture(framebufferTexture, TexCoords - offset).b
+    texture(framebufferTexture, EffectTexCoords + offset).r,
+    texture(framebufferTexture, EffectTexCoords).g,
+    texture(framebufferTexture, EffectTexCoords - offset).b
   );
 
   if (enableExposure){
@@ -93,10 +100,10 @@ void main(){
   color = (color - 0.5) * contrast + 0.5;
 
   // CRT overlay 
-  //float scanlineCount = 140.0;
-  //float flicker = 0.97 + 0.03 * sin(realtime * 8.0);
-  //float scanline =  0.85 + 0.05 * sin(TexCoords.y * scanlineCount * 6.2831853);
-  //color *= scanline * flicker;
+  // SCANLINE EFFECT float scanlineCount = 40.0;
+  // SCANLINE EFFECT float flicker = 0.97 + 0.03 * sin(realtime * 8.0);
+  // SCANLINE EFFECT float scanline =  0.85 + 0.05 * sin(EffectTexCoords.y * scanlineCount * 6.2831853);
+  // SCANLINE EFFECT color *= scanline * flicker;
 
   FragColor = vec4(color, FragColor.a) * vec4(colorGrade.rgb, 1);
 
